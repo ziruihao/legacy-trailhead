0ddac813de509648999d19e8233bd2c9e6f1e332
Samuel Schiff <samuel.b.schiff.20@dartmouth.edu>
2018-04-14 17:28:54 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("starting up!");
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+
+const timer = {
+  time: 0,
+  output() {
+    console.log(this.time);
+    $('#main').html(`You've been on this page for ${this.time} seconds`);
+    this.time += 1;
+  },
+  interval() {
+    setInterval(() => { this.output(); }, 1000);
+  },
+
+};
+
+
+timer.interval();
