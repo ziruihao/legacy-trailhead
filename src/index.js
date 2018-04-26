d9dce7c0c838ad7c41471a0d366fb4bc459bbfc1
Samuel Schiff <samuel.b.schiff.20@dartmouth.edu>
2018-04-26 12:10:41 -0400

    v3

--- src/index.js
+++ src/index.js
@@ -1,21 +1,52 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { Switch } from 'react-router';
+// import $ from 'jquery';
 import './style.scss';
 
-$('#main').html('Here we go!');
 
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
 
-const timer = {
-  time: 0,
-  output() {
-    console.log(this.time);
-    $('#main').html(`You've been on this page for ${this.time} seconds`);
-    this.time += 1;
-  },
-  interval() {
-    setInterval(() => { this.output(); }, 1000);
-  },
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
 
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 
-timer.interval();
+ReactDOM.render(<App />, document.getElementById('main'));
