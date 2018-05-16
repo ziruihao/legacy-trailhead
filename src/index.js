aac495dc20f234b8331d128233768e4162738081
cshashwat <Shashwat.Chaturvedi.18@dartmouth.edu>
2018-05-15 22:44:34 -0400

    Added logic for signin, signout and signup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Switch } from 'react-router';
@@ -13,14 +14,20 @@ import AllTrips from './components/alltrips';
 import CreateTrip from './components/createtrip';
 import MyTrips from './components/mytrips';
 import TripDetails from './components/tripdetails';
+import { ActionTypes } from './actions';
 import './style.scss';
 
 // this creates the store with the reducers, and does some other stuff to initialize devtools
 // boilerplate to copy, don't have to know
 const store = createStore(reducers, {}, compose(
-  applyMiddleware(),
+  applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f,
 ));
+
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch({ type: ActionTypes.AUTH_USER });
+}
 
 const FallBack = (props) => {
   return <div>URL Not Found</div>;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
-
+import AuthReducer from './auth-reducer';
 import TripsReducer from './trips-reducer';
 
 const rootReducer = combineReducers({
+  auth: AuthReducer,
   trip: TripsReducer,
 });
 
