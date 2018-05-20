d264485564e8bb96f2a9f4f882a35426b26fcc18
Isabel Hurley <iah.19@dartmouth.edu>
2018-05-20 19:47:03 -0400

    fixed merge

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 export const ActionTypes = {
   FETCH_TRIPS: 'FETCH_TRIPS',
   FETCH_TRIP: 'FETCH_TRIP',
+  JOIN_TRIP: 'JOIN_TRIP',
   CREATE_TRIP: 'CREATE_TRIP',
   SIGN_UP_TRIP: 'SIGN_UP_TRIP',
   CANCEL_TRIP: 'CANCEL_TRIP',
+  IS_ON_TRIP: 'IS_ON_TRIP',
   AUTH_USER: 'AUTH_USER',
   DEAUTH_USER: 'DEAUTH_USER',
 };
@@ -25,15 +27,26 @@ export function fetchTrips() {
   };
 }
 
-export function fetchTrip(id) {
+export function fetchTrip(tripID) {
   return (dispatch) => {
-    axios.get(`${ROOT_URL}/trip/${id}`).then((response) => {
-      dispatch({
-        type: ActionTypes.FETCH_TRIP,
-        payload: response.data,
-      });
+    axios.get(`${ROOT_URL}/trip/${tripID}`).then((response) => {
+      console.log(response.data);
+      dispatch({ type: ActionTypes.FETCH_TRIP, payload: response.data });
     }).catch((error) => {
+      console.log(error);
       console.log('Fetch trip error');
+    });
+  };
+}
+
+export function joinTrip(tripID) {
+  return (dispatch) => {
+    axios.put(`${ROOT_URL}/jointrip`, { tripID }, { headers: { authorization: localStorage.getItem('token') } }).then((response) => {
+      dispatch({ type: ActionTypes.JOIN_TRIP, payload: response.data.isOnTrip });
+      console.log(response.data);
+    }).catch((error) => {
+      console.log('joinTrip error');
+      console.log(error);
     });
   };
 }
@@ -54,12 +67,32 @@ export function createTrip(trip, history) {
   };
 }
 
+export function cancelTrip(tripID) {
+}
 export function signUpTrip() {
   return {
-    type: ActionTypes.SIGN_UP_TRIP,
+    type: ActionTypes.FETCH_TRIPS,
     payload: null,
   };
+  // return (dispatch) => {
+  //   axios.put(`${ROOT_URL}/joinTrip/${tripID}$`).then((response) => {
+  //     dispatch({ type: ActionTypes.JOIN_TRIP, payload: response.data.isOnTrip });
+  //   }).catch((error) => {
+  //     console.log(error);
+  //   });
+  // };
 }
+
+export function isOnTrip(tripID) {
+  return (dispatch) => {
+    axios.get(`${ROOT_URL}/isOnTrip/${tripID}$`, { headers: { authorization: localStorage.getItem('token') } }).then((response) => {
+      dispatch({ type: ActionTypes.IS_ON_TRIP, payload: response.data.isOnTrip });
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+}
+
 
 export function signIn({ email, password }, history) {
   return (dispatch) => {
