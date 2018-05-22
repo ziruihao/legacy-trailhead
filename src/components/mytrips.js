f123c058111e884b2bf67865381071c70c470236
Isabel Hurley <iah.19@dartmouth.edu>
2018-05-22 15:25:48 -0400

    bootstrap

--- src/components/mytrips.js
+++ src/components/mytrips.js
@@ -15,17 +15,17 @@ class MyTrips extends Component {
   }
 
   renderMyTrips = () => {
+    const style = { width: '18rem' };
     const myTrips =
       this.props.myTrips.map((trip, id) => {
         return (
-          <div key={trip.id}>
-            <a href={`/trip/${trip.id}`}>
-              <div className="myTrip">
-                <h1>{trip.title}</h1>
-                <p>{trip.club}</p>
-                <p>{trip.date}</p>
-              </div>
-            </a>
+          <div key={trip.id} className="card text-center" style={style}>
+            <div className="card-body">
+              <h5 className="card-title">{trip.title}</h5>
+              <p className="card-text">{trip.club}</p>
+              <p className="card-text">{trip.date}</p>
+              <a href={`/trip/${trip.id}`} className="btn btn-primary">See details</a>
+            </div>
           </div>
         );
       });
@@ -35,7 +35,7 @@ class MyTrips extends Component {
   render() {
     console.log(this.props.myTrips);
     return (
-      <div className="myTrips">
+      <div className="card">
         <p>Coming soon: Will show all trips the user is signed up for, and maybe taken </p>
         <NavLink to="/">
           <button>Home</button>
