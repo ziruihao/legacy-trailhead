00cb0da205a893d4cd3d980365f35e0f6d5ae893
Annika Kouhia <annika.j.kouhia.20@dartmouth.edu>
2019-04-28 16:36:57 -0400

    create trip follows figma up to gear, pcard, and vehicles

--- src/components/createtrip.js
+++ src/components/createtrip.js
@@ -11,15 +11,21 @@ class CreateTrip extends Component {
     this.state = {
       title: '',
       leaders: '',
-      // club: '',
+      club: '',
+      experienceNeeded: '',
       description: '',
       startDate: '',
       endDate: '',
+      startTime: '',
+      endTime: '',
+      mileage: '',
+      location: '',
       cost: '',
-      limit: '',
+      length: '',
     };
     this.onFieldChange = this.onFieldChange.bind(this);
     this.createTrip = this.createTrip.bind(this);
+    this.onDateChange = this.onDateChange.bind(this);
   }
 
   componentDidMount() {
@@ -31,45 +37,101 @@ class CreateTrip extends Component {
   }
 
   onFieldChange(event) {
+    console.log(`field changing${event}and name ${event.target.name}and value ${event.target.value}`);
     this.setState({
       [event.target.name]: event.target.value,
     });
   }
 
+  onDateChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+    if (this.state.length === 'single') {
+      this.setState({
+        endDate: event.target.value,
+      });
+    }
+  }
+
   getClubOptions = () => {
-    const options = this.props.clubs.map((club) => {
-      return <option key={club.id} value={club.id}>{club.name}</option>;
+    const listOfClubs = ['Ledyard', 'CNT', 'DMC'];
+    const options = listOfClubs.map((club) => {
+      return <option key={club} value={club}>{club}</option>;
     });
     return options;
   }
 
+  getDateOptions = () => {
+    if (this.state.length === 'multi') {
+      return (
+        <div>
+          <input type="date" name="startDate" onChange={this.onDateChange} className="form-control" value={this.state.startDate} />
+          <input type="date" name="startDate" onChange={this.onDateChange} className="form-control" value={this.state.startDate} />
+        </div>
+      );
+    } else {
+      return (
+        <input type="date" name="startDate" onChange={this.onDateChange} className="form-control" value={this.state.startDate} />
+      );
+    }
+  }
+
+  handleOptionChange = (changeEvent) => {
+    if (changeEvent.target.value === 'Yes') {
+      this.setState({
+        experienceNeeded: true,
+      });
+    } else {
+      this.setState({
+        experienceNeeded: false,
+      });
+    }
+  };
+
+  handleDateChange = (changeEvent) => {
+    if (changeEvent.target.value === 'single') {
+      this.setState({
+        length: 'single',
+      });
+    } else {
+      this.setState({
+        length: 'multi',
+      });
+    }
+  };
+
   createTrip() {
-    // const defaultClub = () => {
-    //   for (const club of this.props.clubs) {
-    //     if (club.name === 'Ledyard') {
-    //       return club.id;
-    //     }
-    //   }
-    //   return '';
-    // };
-
-    console.log('1creatign trip from createtrip.js');
-
     const trip = {
       title: this.state.title,
       leaders: this.state.leaders.trim().split(','),
-      // club: this.state.club !== '' ? this.state.club : defaultClub(),
-      club: 'Ledyard',
+      club: this.state.club,
+      experienceNeeded: this.state.experienceNeeded,
       description: this.state.description,
+      mileage: this.state.mileage,
+      location: this.state.location,
       startDate: this.state.startDate,
       endDate: this.state.endDate,
+      startTime: this.state.startTime,
+      endTime: this.state.endTime,
       cost: this.state.cost,
-      limit: this.state.limit,
     };
-    console.log('2creatign trip from createtrip.js');
 
+    if (!trip.startDate) {
+      console.log('startdate');
+    }
+    if (!trip.endDate) {
+      console.log('endDate');
+    }
+    if (!trip.startTime) {
+      console.log('startTime');
+    }
+    if (!trip.endTime) {
+      console.log('endTime');
+    }
     // Validate input
-    if (!(trip.title && trip.leaders && trip.club && trip.description && trip.startDate && trip.endDate && trip.cost && trip.limit)) {
+    if (!(trip.title && trip.leaders && trip.club && trip.description && trip.startDate && trip.endDate && trip.startTime && trip.endTime
+      && trip.cost && trip.experienceNeeded && trip.mileage && trip.location)) {
       this.props.appError('All trip fields must be filled out');
       return;
     }
@@ -105,14 +167,55 @@ class CreateTrip extends Component {
           <select name="club" className="custom-select field" defaultValue="Ledyard" onChange={this.onFieldChange}>
             {this.getClubOptions()}
           </select>
-          <input className="form-control field" type="number" onChange={this.onFieldChange} name="limit" placeholder="Max # of people (e.g. 8, 10, etc)" value={this.state.limit} />
+          <div> Experience Needed </div>
+          <form>
+            <input
+              type="radio"
+              value="Yes"
+              onChange={this.handleOptionChange}
+              checked={this.state.experienceNeeded === true}
+            />
+              Yes
+            <input
+              type="radio"
+              value="No"
+              onChange={this.handleOptionChange}
+              checked={this.state.experienceNeeded === false}
+            />
+              No
+          </form>
           <textarea className="form-control field" onChange={this.onFieldChange} name="description" placeholder="Trip description (markdown supported)" value={this.state.description} />
+          <input onChange={this.onFieldChange} name="mileage" placeholder="Estimated mileage" value={this.state.mileage} />
+          <input onChange={this.onFieldChange} name="location" placeholder="Location" value={this.state.location} />
+          <div> Trip Duration </div>
+          <form>
+            <input
+              type="radio"
+              value="single"
+              onChange={this.handleDateChange}
+              checked={this.state.length === 'single'}
+            />
+              Single Day
+            <input
+              type="radio"
+              value="multi"
+              onChange={this.handleDateChange}
+              checked={this.state.length === 'multi'}
+            />
+              Multi-Day
+          </form>
           <div className="input-group field">
             <div className="input-group-prepend">
               <span className="input-group-text">Start Date and End Date</span>
             </div>
-            <input type="date" name="startDate" onChange={this.onFieldChange} className="form-control" value={this.state.startDate} />
-            <input type="date" name="endDate" onChange={this.onFieldChange} className="form-control" value={this.state.endDate} />
+            {this.getDateOptions()}
+          </div>
+          <div className="input-group field">
+            <div className="input-group-prepend">
+              <span className="input-group-text">Start Time and End Time</span>
+            </div>
+            <input type="time" name="startTime" onChange={this.onFieldChange} className="form-control" value={this.state.startTime} />
+            <input type="time" name="endTime" onChange={this.onFieldChange} className="form-control" value={this.state.endTime} />
           </div>
           <div className="input-group field">
             <div className="input-group-prepend">
