0b5c932c42789c61330884f784fc42da5a3ae215
prosper007 <onungwap@gmail.com>
2019-07-03 16:22:08 -0400

    driver cert form

--- src/components/profilepage.js
+++ src/components/profilepage.js
@@ -7,13 +7,23 @@ import { updateUser, getClubs } from '../actions';
 import '../styles/profilepage-style.scss';
 
 class ProfilePage extends Component {
+  TRAILER_CONSTANT = 'TRAILER';
+
+  CERTIFICATION_CONSTANT = 'CERTIFICATION';
+
+  NONE_CONSTANT = 'NONE';
+
+  CERTIFICATIONS = [null, 'MICROBUS', 'VAN', 'MINIVAN'];
+
   constructor(props) {
     super(props);
     this.state = {
       email: '',
       name: '',
-      clubsList: [],
       dash_number: '',
+      clubsList: [],
+      driver_cert: null,
+      trailer_cert: false,
       isEditing: false,
     };
     this.onFieldChange = this.onFieldChange.bind(this);
@@ -50,13 +60,26 @@ class ProfilePage extends Component {
     }
   }
 
-  getUpdatedVals = () => {
+  onDriverCertChange = (event) => {
+    event.persist();
+    if (event.target.name === this.TRAILER_CONSTANT) {
+      this.setState({ trailer_cert: event.target.checked });
+    }
+    if (event.target.name === this.CERTIFICATION_CONSTANT) {
+      const selected = event.target.value === this.NONE_CONSTANT ? null : event.target.value;
+      this.setState({ driver_cert: selected });
+    }
+  }
+
+  startEditing = () => {
     this.setState({
       name: this.props.user.name,
       email: this.props.user.email,
       dash_number: this.props.user.dash_number ? this.props.user.dash_number : '',
-      isEditing: true,
       clubsList: this.props.user.leader_for,
+      driver_cert: this.props.user.driver_cert,
+      trailer_cert: this.props.user.trailer_cert,
+      isEditing: true,
     });
   }
 
@@ -69,10 +92,40 @@ class ProfilePage extends Component {
       this.props.user.leader_for.forEach((club) => {
         clubString = clubString.concat(`${club.name}, `);
       });
-      const clubs = clubString.substring(0, clubString.length - 2);
-      return clubs.length === 0 ? <em>None</em> : clubs;
+      const clubs = clubString.length - 2 <= 0
+        ? <em>None</em> : clubString.substring(0, clubString.length - 2);
+      return (
+        <div>
+          <h5 className="card-title">DOC clubs that you are a leader for</h5>
+          {clubs}
+        </div>
+      );
     } else {
       return this.getClubForm();
+    }
+  }
+
+  displayCertifications = () => {
+    if (!this.state.isEditing) {
+      let certifications = '';
+      if (this.props.user.driver_cert === null && !this.props.user.trailer_cert) {
+        certifications = this.NONE_CONSTANT;
+      } else {
+        certifications = this.props.user.trailer_cert ? `${this.props.driver_cert}, ${this.TRAILER_CONSTANT}` : this.props.driver_cert;
+      }
+      return (
+        <div>
+          <h5 className="card-title">Driver certifications</h5>
+          <p>{certifications}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h5 className="card-title">Driver certifications</h5>
+          {this.getCertificationsForm()}
+        </div>
+      );
     }
   }
 
@@ -81,7 +134,7 @@ class ProfilePage extends Component {
       return <h1>You can&apos;t update this until your previous changes have been reviewed</h1>;
     }
     const currentClubIds = this.state.clubsList.map(club => club._id);
-    return this.props.clubs.map((club) => {
+    const clubForm = this.props.clubs.map((club) => {
       const checked = currentClubIds.includes(club.id);
       return (
         <div key={club.id}>
@@ -99,22 +152,99 @@ class ProfilePage extends Component {
         </div>
       );
     });
+    return (
+      <div>
+        <h5 className="card-title"> {this.props.user.role === 'Leader' ? 'DOC clubs that you are a leader for' : 'Request leader access'}</h5>
+        {clubForm}
+      </div>
+    );
   }
 
-  displayFeedback = () => {
+  getCertificationsForm = () => {
+    if (this.props.user.has_pending_changes) {
+      return <h1>You can&apos;t update this until your previous changes have been reviewed</h1>;
+    }
+
+    const trailer = (
+      <div>
+        <label htmlFor={this.TRAILER_CONSTANT}>
+          <input
+            type="checkbox"
+            name={this.TRAILER_CONSTANT}
+            id={this.TRAILER_CONSTANT}
+            value={this.TRAILER_CONSTANT}
+            onChange={this.onDriverCertChange}
+            checked={this.state.trailer_cert}
+          />
+          {this.TRAILER_CONSTANT}
+        </label>
+      </div>
+    );
+
+    const certificationForm = this.CERTIFICATIONS.map((certification) => {
+      const checked = certification === this.state.driver_cert;
+      certification = certification === null ? this.NONE_CONSTANT : certification;
+      return (
+        <div key={certification}>
+          <label htmlFor={certification}>
+            <input
+              type="radio"
+              name={this.CERTIFICATION_CONSTANT}
+              id={certification}
+              value={certification}
+              onChange={this.onDriverCertChange}
+              checked={checked}
+            />
+            {certification}
+          </label>
+        </div>
+      );
+    });
+    return (
+      <div>
+        {certificationForm}
+        {trailer}
+      </div>
+    );
+  }
+
+  displayLeaderFeedback = () => {
     if (this.props.user.role === 'Leader' && this.state.clubsList.length === 0) {
       return (
         <p>
-          {`Unchecking all clubs will revoke your leader permissions.
+          Unchecking all clubs will revoke your leader permissions.
           You will need to request acccess from the OPO to regain them.
-          Please review before you proceed`}.
+          Please review before you proceed.
         </p>
       );
     } else if (this.props.user.role === 'Trippee' && this.state.clubsList.length > 0) {
       return (
         <p>
-          {`Submitting this form will trigger a request to the OPO for leader permissions. 
-          Please proceed only if you're a leader!`}
+          Submitting this form will trigger a request to the OPO for leader permissions.
+          Please proceed only if you are a leader in the selected clubs!
+        </p>
+      );
+    } else {
+      return null;
+    }
+  }
+
+  displayCertificationFeedback = () => {
+    if ((this.props.user.trailer_cert && !this.state.trailer_cert)
+      || (this.props.user.driver_cert !== null && this.state.driver_cert === null)) {
+      return (
+        <p>
+          Submitting this form will revoke one or more of your driver certifications.
+          You will need approval from the OPO to regain them.
+          Please review before you proceed.
+        </p>
+      );
+    } else if ((!this.props.user.trailer_cert && this.state.trailer_cert)
+      || (this.props.user.driver_cert !== this.state.driver_cert)) {
+      return (
+        <p>
+          Submitting this form will trigger a request to the OPO for approval.
+          Please proceed only if you have the selected driver certifications.
         </p>
       );
     } else {
@@ -134,9 +264,11 @@ class ProfilePage extends Component {
       name: this.state.name,
       leader_for: this.state.clubsList,
       dash_number: this.state.dash_number,
+      driver_cert: this.state.driver_cert,
+      trailer_cert: this.state.trailer_cert,
     };
-    this.setState({ isEditing: false, clubsList: [] });
     this.props.updateUser(updatedUser);
+    this.setState({ isEditing: false });
   }
 
   changeToOPO(event) {
@@ -195,10 +327,12 @@ class ProfilePage extends Component {
                 <p className="card-text">{this.props.user.dash_number ? this.props.user.dash_number : 'Please fill out'}</p>
               </div>
               <div className="profile-field">
-                <h5 className="card-title">DOC clubs that you are a leader for</h5>
-                <p className="card-text">{this.displayClubs()}</p>
+                {this.displayClubs()}
               </div>
-              <button className="btn btn-primary" onClick={this.getUpdatedVals}>Update my info</button>
+              <div className="profile-field">
+                {this.displayCertifications()}
+              </div>
+              <button className="btn btn-primary" onClick={this.startEditing}>Update my info</button>
               {this.pendingChanges()}
             </div>
           </div>
@@ -224,9 +358,10 @@ class ProfilePage extends Component {
               <input type="text" name="dash_number" onChange={this.onFieldChange} className="form-control" value={this.state.dash_number} />
             </div>
             <div className="profile-field">
-              <h5 className="card-title"> {this.props.user.role === 'Leader' ? 'DOC clubs that you are a leader for' : 'Request leader access'}</h5>
               {this.displayClubs()}
-              {this.displayFeedback()}
+              {this.displayLeaderFeedback()}
+              {this.displayCertifications()}
+              {this.displayCertificationFeedback()}
             </div>
 
             <button className="btn btn-primary" onClick={this.changeToOPO}>Change to OPO</button>
