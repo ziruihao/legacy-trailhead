7c0591df3dc30f6873c572c7931606884fa86390
Emma Rafkin <emmarafkin@gmail.com>
2019-07-11 14:52:38 -0400

    added an all trips option to theexperience needed dropdown

--- src/components/alltrips.js
+++ src/components/alltrips.js
@@ -12,12 +12,10 @@ class AllTrips extends Component {
     super(props);
     this.state = {
       club: '',
-      beginner: true,
+      beginner: "all",
       grid: true,
+      showTrip:"",
     };
-
-    this.onGrid = this.onGrid.bind(this);
-    this.onList = this.onList.bind(this);
   }
 
   componentDidMount(props) {
@@ -28,13 +26,8 @@ class AllTrips extends Component {
     this.props.fetchTrips();
     this.props.getClubs();
   }
-
-  onGrid() {
-    this.setState({ grid: true });
-  }
-
-  onList() {
-    this.setState({ grid: false });
+  showTrip(tripID){
+    this.setState({ showTrip: tripID});
   }
 
   formatDate = (date) => {
@@ -81,50 +74,80 @@ class AllTrips extends Component {
           name="select"
           className="custom-select all-trips-select"
           defaultValue=""
-          onChange={(e) => {
-
-              let bool = true;
-              if (e.target.value === "no") {
-                bool = false;
-              }
-              this.setState({ beginner: bool }); }}
+          onChange={(e) => {this.setState({ beginner: e.target.value }); }}
         >
+          <option key = "all" value = "all"> All Trips </option>
           <option key="yes" value="yes">Yes</option>
           <option key="no" value="no">No</option>
         </select>
     );
   }
+
   renderTrips = () => {
     const sortedTrips = this.props.trips.sort(this.compareStartDates);
-    const tripsGrid = sortedTrips.filter(trip => (this.state.club === '' || trip.club.name === this.state.club ) && trip.experienceNeeded !== this.state.beginner).map((trip) => {
-      let card_id = trip.club.name;
-      if(card_id==="Cabin and Trail") card_id = "cnt";
-      if(card_id==="Women in the Wilderness") card_id = "wiw";
-      if(card_id==="Surf Club") card_id = "surf";
-      if(card_id==="Mountain Biking") card_id = "dmbc";
-      if(card_id==="Winter Sports") card_id = "wsc";
+    let tripsGrid = sortedTrips;
+    if(this.state.beginner === "all") {
+       tripsGrid = sortedTrips.filter(trip => (this.state.club === '' || trip.club.name === this.state.club )).map((trip) => {
+        let card_id = trip.club.name;
+        if(card_id==="Cabin and Trail") card_id = "cnt";
+        if(card_id==="Women in the Wilderness") card_id = "wiw";
+        if(card_id==="Surf Club") card_id = "surf";
+        if(card_id==="Mountain Biking") card_id = "dmbc";
+        if(card_id==="Winter Sports") card_id = "wsc";
 
+        // TODO: make this less specific
+        if(trip.club.name === 'Bait and Bullet' || trip.club.name === 'Other' ) card_id = "doc";
+          return (
+            <div key={trip.id} className="card all-trips-card text-center card-trip margins">
+              <NavLink to={`/trip/${trip.id}`} key={trip.id}>
+                <div className="all-trips-card-body" id = {card_id}>
+                  <h2 className="card-title">{trip.title}</h2>
+                  <p className="card-text">{this.formatDate(trip.startDate)} - {this.formatDate(trip.endDate)}</p>
+                  <p className="card-text">{this.formatDescription(trip.description)}</p>
+                  <p className="card-club">{trip.club ? trip.club.name : ''}</p>
 
-
-
-
-      // TODO: make this less specific
-      if(trip.club.name === 'Bait and Bullet' || trip.club.name === 'Other' ) card_id = "doc";
-      return (
-        <div key={trip.id} className="card all-trips-card text-center card-trip margins">
-          <NavLink to={`/trip/${trip.id}`} key={trip.id}>
-
-            <div className="card-body" id = {card_id}>
-              <h2 className="card-title">{trip.title}</h2>
-              <p className="card-text">{this.formatDate(trip.startDate)} - {this.formatDate(trip.endDate)}</p>
-              <p className="card-text">{this.formatDescription(trip.description)}</p>
-              <p className="card-club">{trip.club ? trip.club.name : ''}</p>
-
+                </div>
+              </NavLink>
             </div>
-          </NavLink>
-        </div>
-      );
-    });
+          );
+
+
+      });
+    }else{
+      let experienceNeeded = "";
+      if(this.state.beginner === "yes"){
+        experienceNeeded= false;
+      }else{
+        experienceNeeded = true;
+      }
+      tripsGrid = sortedTrips.filter(trip => (this.state.club === '' || trip.club.name === this.state.club )&& trip.experienceNeeded===experienceNeeded).map((trip) => {
+       let card_id = trip.club.name;
+       if(card_id==="Cabin and Trail") card_id = "cnt";
+       if(card_id==="Women in the Wilderness") card_id = "wiw";
+       if(card_id==="Surf Club") card_id = "surf";
+       if(card_id==="Mountain Biking") card_id = "dmbc";
+       if(card_id==="Winter Sports") card_id = "wsc";
+
+       // TODO: make this less specific
+       if(trip.club.name === 'Bait and Bullet' || trip.club.name === 'Other' ) card_id = "doc";
+         return (
+           <div key={trip.id} className="card all-trips-card text-center card-trip margins">
+             <NavLink to={`/trip/${trip.id}`} key={trip.id}>
+               <div className="all-trips-card-body" id = {card_id}>
+                 <h2 className="card-title">{trip.title}</h2>
+                 <p className="card-text">{this.formatDate(trip.startDate)} - {this.formatDate(trip.endDate)}</p>
+                 <p className="card-text">{this.formatDescription(trip.description)}</p>
+                 <p className="card-club">{trip.club ? trip.club.name : ''}</p>
+
+               </div>
+             </NavLink>
+           </div>
+         );
+
+
+     });
+    }
+
 
     if (tripsGrid.length === 0) {
       return <div>No upcoming trips for this club</div>;
@@ -138,19 +161,20 @@ class AllTrips extends Component {
   }
 
   render() {
-    return (
-      <div className="all-trips">
-        <h1 className="all-trips-header">All Trips</h1>
-        <div className = "all-trips-dropdown-bar">
-          <div className = "all-trips-dropdown-header"> Beginner: </div>  {this.renderBeginnerDropdown()}
-
-          <div className = "all-trips-dropdown-header"> Subclub: </div>  {this.renderClubDropdown()}
+    if(this.state.showTrip===""){
+      return (
+        <div className="all-trips">
+          <h1 className="all-trips-header">All Trips</h1>
+          <div className = "all-trips-dropdown-bar">
+            <div className = "all-trips-dropdown-header"> Beginner: </div>  {this.renderBeginnerDropdown()}
+            <div className = "all-trips-dropdown-header"> Subclub: </div>  {this.renderClubDropdown()}
+          </div>
+          <div className="all-trips-box">
+            {this.renderTrips()}
+          </div>
         </div>
-        <div className="all-trips-box">
-          {this.renderTrips()}
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
