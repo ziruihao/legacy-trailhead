261d311b7ed73d9ea57f1a379c04f1c1cc6d348c
prosper007 <onungwap@gmail.com>
2019-07-15 15:02:06 -0400

    pause tripdetail refactor

--- src/components/tripdetails.js
+++ src/components/tripdetails.js
@@ -1,10 +1,7 @@
-/* eslint-disable react/button-has-type */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter, NavLink } from 'react-router-dom';
-// import marked from 'marked';
-import { joinTrip, addToPending, fetchTrip, leaveTrip, isOnTrip, emailTrip, deleteTrip } from '../actions';
-import '../styles/tripdetails-style.scss';
+import { withRouter } from 'react-router-dom';
+import { fetchTrip, joinTrip, moveToPending, deleteTrip, addToPending, editUserGear, leaveTrip, appError } from '../actions';
 import TripeeTripDetails from './tripdetails_trippee';
 import LeaderTripDetails from './tripdetails_leader';
 
@@ -13,415 +10,233 @@ class TripDetails extends Component {
     super(props);
 
     this.state = ({
-      showEmail: false,
-      emailSubject: '',
-      emailBody: '',
-      hasSignedUp: false,
+      pendingEmail: '',
+      onTripEmail: '',
+      showLeaderModal: false,
       trippeeGear: [],
+      isEditing: true,
+      showTrippeeModal: false,
     });
-    this.onFieldChange = this.onFieldChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onJoin = this.onJoin.bind(this);
-    this.onLeave = this.onLeave.bind(this);
-    this.onEmail = this.onEmail.bind(this);
-    this.toggleEmail = this.toggleEmail.bind(this);
-    this.onPending = this.onPending.bind(this);
+    this.onGearChange = this.onGearChange.bind(this);
+    this.goBack = this.goBack.bind(this);
+    this.onTextChange = this.onTextChange.bind(this);
+    this.pendingEmailRef = React.createRef();
+    this.onTripEmailRef = React.createRef();
+    ref = {pendingEmailRef: this.pendingEmailRef, onTripEmailRef: this.onTripEmailRef}
   }
 
-  async componentDidMount() {
-    return Promise
-      .all([
-        this.props.isOnTrip(this.props.match.params.tripID),
-        this.props.fetchTrip(this.props.match.params.tripID),
-      ])
-      .catch((error) => {
-        console.log(error);
+  componentDidMount() {
+    this.props.fetchTrip(this.props.match.params.tripID)
+      .then(() => {
+        if (this.props.isLeaderOnTrip) {
+          let pendingEmail = '';
+          let onTripEmail = '';
+          this.props.trip.pending.forEach((pender) => {
+            pendingEmail += `${pender.user.email}, `;
+          });
+          this.props.trip.members.forEach((member) => {
+            onTripEmail += `${member.user.email}, `;
+          });
+          pendingEmail = pendingEmail.substring(0, pendingEmail.length - 2);
+          onTripEmail = onTripEmail.substring(0, onTripEmail.length - 2);
+          this.setState({ pendingEmail, onTripEmail });
+        } else if (this.props.userTripStatus === 'PENDING') {
+          this.props.trip.pending.some((pender) => {
+            if (pender.user._id === this.props.user.id) {
+              this.setState({ trippeeGear: pender.gear, isEditing: false });
+            }
+            return pender.user._id === this.props.user.id;
+          });
+        } else if (this.props.userTripStatus === 'APPROVED') {
+          this.props.trip.members.some((member) => {
+            if (member.user._id === this.props.user.id) {
+              this.setState({ trippeeGear: member.gear, isEditing: false });
+            }
+            return member.user._id === this.props.user.id;
+          });
+        }
       });
   }
 
-  onFieldChange(event) {
+  onGearChange(event) {
     event.persist();
-    if (event.target.type === 'checkbox') {
-      if (event.target.checked) {
-        this.setState(prevState => ({
-          trippeeGear: [...prevState.trippeeGear, { gearId: event.target.dataset.id, gear: event.target.dataset.gear }],
-        }));
-      } else {
-        this.setState((prevState) => {
-          const withoutClickedGear = prevState.trippeeGear.filter(gear => gear.gearId !== event.target.dataset.id);
-          return {
-            trippeeGear: withoutClickedGear,
-          };
-        });
-      }
+    if (event.target.checked) {
+      this.setState(prevState => ({
+        trippeeGear: [...prevState.trippeeGear, { gearId: event.target.dataset.id, gear: event.target.dataset.gear }],
+      }));
     } else {
-      this.setState({
-        [event.target.name]: event.target.value,
+      this.setState((prevState) => {
+        const withoutClickedGear = prevState.trippeeGear.filter(gear => gear.gearId !== event.target.dataset.id);
+        return {
+          trippeeGear: withoutClickedGear,
+        };
       });
     }
   }
 
-  onSubmit() {
+  startEditing = () => {
+    this.setState({ isEditing: true });
+  }
+
+  cancelChanges = () => {
+    this.props.trip.pending.some((pender) => {
+      if (pender.user._id === this.props.user.id) {
+        this.setState({ trippeeGear: pender.gear, isEditing: false });
+      }
+      return pender.user._id === this.props.user.id;
+    });
+  }
+
+  activateTrippeeModal = () => {
+    this.setState({ showTrippeeModal: true });
+  }
+
+  activateLeaderModal = () => {
+    this.setState({ showLeaderModal: true });
+  }
+
+  closeTrippeeModal = () => {
+    this.setState({ showTrippeeModal: false });
+  }
+
+  closeLeaderModal = () => {
+    this.setState({ showLeaderModal: false });
+  }
+
+  goBack = () => {
+    this.props.history.goBack();
+  }
+
+  cancelSignup = () => {
+    const cancelPromise = new Promise((resolve, reject) => {
+      this.props.leaveTrip(this.props.trip._id, this.props.userTripStatus);
+      resolve();
+    });
+    cancelPromise.then(() => {
+      this.setState({ showTrippeeModal: false, trippeeGear: [] });
+      window.scrollTo(0, 0);
+    });
+  }
+
+  signUp = () => {
     if (!this.props.user.email || !this.props.user.name || !this.props.user.dash_number) {
-      alert('Please fill out all of your info before signing up');
+      this.props.appError('Please fill out all of your info before signing up');
       this.props.history.push('/user');
-    } else if (!this.onPending()) {
+    } else {
       const signUpInfo = {
         id: this.props.trip._id,
         trippeeGear: this.state.trippeeGear,
       };
-      this.props.addToPending(signUpInfo);
-      this.setState({ hasSignedUp: true });
-      this.props.fetchTrip(this.props.match.params.tripID);
-    } else {
-      alert('You are already on the pending list.');
+      const signupPromise = new Promise((resolve, reject) => {
+        this.props.addToPending(signUpInfo);
+        resolve();
+      });
+      signupPromise.then(() => {
+        this.setState({ isEditing: false });
+        window.scrollTo(0, 0);
+      });
     }
   }
 
-  onPending() {
-    let isPending = false;
-    this.props.trip.pending.some((pender) => {
-      if (pender.user.id === this.props.user.id) {
-        isPending = true;
-      }
-      return isPending;
+  editGear = () => {
+    const signUpInfo = {
+      id: this.props.trip._id,
+      trippeeGear: this.state.trippeeGear,
+    };
+    const editPromise = new Promise((resolve, reject) => {
+      this.props.editUserGear(signUpInfo);
+      resolve();
     });
-    if (!isPending && !this.state.hasSignedUp) {
-      return false;
-    }
-    return true;
-  }
-
-  onJoin(pend) {
-    if (!this.props.user.email || !this.props.user.name || !this.props.user.dash_number) {
-      alert('Please fill out all of your info before signing up');
-      this.props.history.push('/user');
-    } else {
-      this.props.joinTrip(this.props.trip._id, pend);
-      this.props.fetchTrip(this.props.match.params.tripID);
-    }
-  }
-
-  onLeave() {
-    this.props.leaveTrip(this.props.trip._id);
-    this.props.fetchTrip(this.props.match.params.tripID);
-  }
-
-  onEmail() {
-    this.props.emailTrip(this.props.trip._id, this.state.emailSubject, this.state.emailBody, this.props.history);
-    this.setState({ showEmail: false });
-  }
-
-  getLeaders = (leaders) => {
-    if (leaders) {
-      let leaderString = '';
-      for (const leader of leaders) {
-        leaderString = leaderString.concat(`${leader.name} (${leader.email}), `);
-      }
-      return leaderString.substring(0, leaderString.length - 2);
-    }
-    return '';
-  }
-
-  formatDate = (date) => {
-    // date fix adapted from https://stackoverflow.com/questions/7556591/javascript-date-object-always-one-day-off/31732581
-    if (!date) {
-      return '';
-    }
-    return new Date(date.replace(/-/g, '/').replace(/T.+/, '')).toLocaleDateString('en-US');
-  }
-
-  showMembers = () => {
-    if (!this.props.trip.leaders) {
-      return <span />;
-    }
-
-    if (this.props.trip.members.length < 1) {
-      return (<p> No Members Yet </p>);
-    }
-
-    const rows = this.props.trip.members.map((member) => {
-      return (
-        <tr key={member._id}>
-          <td>{member.user.name}</td>
-          <td>{member.user.email}</td>
-          <td>{member.user.dash_number}</td>
-          <td>{member.gear.map(gear => (
-            <li key={gear._id}>{gear.gear}</li>
-          ))}
-          </td>
-        </tr>
-      );
+    editPromise.then(() => {
+      this.setState({ isEditing: false });
     });
-
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Dash #</th>
-            <th scope="col">Requests</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
-    );
   }
 
-  showPending = () => {
-    if (!this.props.trip.leaders) {
-      return <span />;
-    }
+  moveToTrip = (pender) => {
+    this.props.joinTrip(this.props.trip._id, pender);
+  }
 
-    if (this.props.trip.pending.length < 1) {
-      return (<p> No Pending Yet </p>);
-    }
+  moveToPending = (member) => {
+    this.props.moveToPending(this.props.trip._id, member);
+  }
 
-    const rows = this.props.trip.pending.map((pend) => {
-      return (
-        <tr key={pend._id}>
-          <td>{pend.user.name}</td>
-          <td>{pend.user.email}</td>
-          <td>{pend.gear.map(gear => (
-            <li key={gear._id}>{gear.gear}</li>
-          ))}
-          </td>
-          <td><button type="button" className="btn btn-success btn-email" onClick={() => this.onJoin(pend)}>Add To Trip</button></td>
-        </tr>
-      );
+  copyPendingToClip = (event) => {
+    this.pendingEmail.select();
+    document.execCommand('copy');
+    event.target.focus();
+  }
+  
+  onTextChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
     });
-
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Requests</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
-    );
   }
 
-  showEmail = () => {
-    return (
-      <div id="email">
-        <p>Subject</p>
-        <input name="emailSubject" className="email subject" type="text" onChange={this.onFieldChange} value={this.state.emailSubject} />
-        <p>Body</p>
-        <textarea name="emailBody" className="email body" type="text" onChange={this.onFieldChange} value={this.state.emailBody} />
-        <button type="button" className="btn btn-success btn-email" onClick={this.onEmail}>Send the email</button>
-      </div>
-    );
+  isObjectEmpty = (object) => {
+    return Object.entries(object).length === 0 && object.constructor === Object;
   }
 
-  getMemberList = () => {
-    if (!this.props.trip.leaders) {
-      return <span />;
-    }
-    let isLeaderForTrip = false;
-    this.props.trip.leaders.some((leader) => {
-      if (leader.id === this.props.user.id) {
-        isLeaderForTrip = true;
-      }
-      return leader.id === this.props.user.id;
-    });
-    if (isLeaderForTrip) {
-      return (
-        <div>
-          <div>
-            <h3> Members </h3>
-            {this.showMembers()}
-          </div>
-          <div>
-            <h3> Pending </h3>
-            {this.showPending()}
-          </div>
-        </div>
-      );
-    } else {
-      return null;
-    }
-  }
-
-  getGearRequests = () => {
-    if (!this.props.trip.leaders) {
-      return <span />;
-    }
-    let isLeaderForTrip = false;
-    this.props.trip.leaders.some((leader) => {
-      if (leader.id === this.props.user.id) {
-        isLeaderForTrip = true;
-      }
-      return leader.id === this.props.user.id;
-    });
-    if (isLeaderForTrip) {
-      let gearRequests, requestStatus;
-      if (this.props.trip.OPOGearRequests.length === 0) {
-        gearRequests = <span>None</span>;
-        requestStatus = null;
-      } else {
-        gearRequests = this.props.trip.OPOGearRequests.map(gearrequest => (
-          <li key={gearrequest}>{gearrequest}</li>
-        ));
-        requestStatus = <h3>Request Status: {this.props.trip.gearStatus}</h3>;
-      }
-      return (
-        <div>
-          <h3>Gear Requests:</h3>
-          {gearRequests}
-          {requestStatus}
-        </div>
-      );
-    } else {
-      return null;
-    }
-  }
-
-  getTrippeeGear = () => {
-    if (!this.props.trip.leaders) {
-      return <span />;
-    }
-    let isLeaderForTrip = false;
-    this.props.trip.leaders.some((leader) => {
-      if (leader.id === this.props.user.id) {
-        isLeaderForTrip = true;
-      }
-      return leader.id === this.props.user.id;
-    });
-    if (isLeaderForTrip) {
-      let trippeeGear, requestStatus;
-      if (this.props.trip.trippeeGear.length === 0) {
-        trippeeGear = <span>None</span>;
-        requestStatus = null;
-      } else {
-        trippeeGear = this.props.trip.trippeeGear.map(gearrequest => (
-          <li key={gearrequest.gear}>{gearrequest.gear} - {gearrequest.quantity}</li>
-        ));
-        requestStatus = <h3>Trippee Gear Status: {this.props.trip.trippeeGearStatus}</h3>;
-      }
-      return (
-        <div>
-          <h3>Trippe Gear:</h3>
-          {trippeeGear}
-          {requestStatus}
-        </div>
-      );
-    } else if (!this.onPending() && !this.props.isUserOnTrip && this.props.trip.trippeeGear.length > 0) {
-      return (
-        <div>
-          <h3>Need any of these?</h3>
-          {this.props.trip.trippeeGear.map(gearrequest => (
-            <div key={gearrequest._id}>
-              <label htmlFor={gearrequest._id}>
-                <input
-                  type="checkbox"
-                  name="gear"
-                  data-id={gearrequest._id}
-                  data-gear={gearrequest.gear}
-                  onChange={this.onFieldChange}
-                />
-                {gearrequest.gear}
-              </label>
-            </div>
-          ))}
-        </div>
-      );
-    } else {
-      return null;
-    }
-  }
-
-  appropriateButton = () => {
-    if (!this.props.trip.leaders) {
-      return <span />;
-    }
-
-    let isLeaderForTrip = false;
-    this.props.trip.leaders.forEach((leader) => {
-      if (leader.id === this.props.user.id) {
-        isLeaderForTrip = true;
-      }
-    });
-
-    if (isLeaderForTrip) {
-      return (
-        <div>
-          <h3 className="member-button" onClick={this.toggleEmail}> Send Email To Your Trip </h3>
-          {this.state.showEmail
-            ? (
-              this.showEmail()
-            ) : (
-              <p />
-            )
-          }
-          <NavLink to={`/edittrip/${this.props.trip.id}`} className="edit-button"><button className="btn btn-success">Edit Trip</button></NavLink>
-          <button onClick={this.handleDelete} className="btn btn-danger">Delete Trip</button>
-        </div>
-      );
-    } else if (this.props.isUserOnTrip) {
-      return <button className="btn btn-danger" type="button" onClick={this.onLeave}>Leave Trip</button>;
-    } else if (this.onPending()) {
-      return <div>Your membership is pending</div>;
-    } else {
-      return <button className="btn btn-primary" type="button" onClick={this.onSubmit}>Sign Up</button>;
-    }
-  }
-
-  handleDelete = () => {
-    this.props.deleteTrip(this.props.trip, this.props.history);
-  }
-
-  toggleEmail() {
-    // eslint-disable-next-line react/no-access-state-in-setstate
-    const next = !this.state.showEmail;
-    this.setState({ showEmail: next });
+  deleteTrip = () => {
+    this.props.deleteTrip(this.props.trip.id, this.props.history);
   }
 
   render() {
-    if (this.props.user.role !== 'Leader') {
+    if (!this.isObjectEmpty(this.props.trip)) {
+      const appropriateComponent = this.props.isLeaderOnTrip
+        ? (
+          <LeaderTripDetails
+            trip={this.props.trip}
+            onTripEmail={this.state.onTripEmail}
+            pendingEmail={this.state.pendingEmail}
+            showModal={this.state.showLeaderModal}
+            onTextChange={this.onTextChange}
+            activateLeaderModal={this.activateLeaderModal}
+            closeLeaderModal={this.closeLeaderModal}
+            deleteTrip={this.deleteTrip}
+            moveToTrip={this.moveToTrip}
+            ref={this.ref}
+          />
+        )
+        : (
+          <TripeeTripDetails
+            trip={this.props.trip}
+            trippeeGear={this.state.trippeeGear}
+            isEditing={this.state.isEditing}
+            showModal={this.state.showTrippeeModal}
+            onGearChange={this.onGearChange}
+            startEditing={this.startEditing}
+            cancelChanges={this.cancelChanges}
+            activateTrippeeModal={this.activateTrippeeModal}
+            closeTrippeeModal={this.closeTrippeeModal}
+            cancelSignup={this.cancelSignup}
+            signUp={this.signUp}
+            editGear={this.editGear}
+          />
+        );
       return (
-        <TripeeTripDetails />
-      );
+        appropriateComponent
+      ); 
     } else {
       return (
-        <LeaderTripDetails />
-        // <div className="trip-detail-div">
-        //   <h1> {this.props.trip.title} </h1>
-        //   <h2> Leaders:</h2>
-        //   <p className="leaders"> {this.getLeaders(this.props.trip.leaders)}</p>
-        //   <h3> Dates: {`${this.formatDate(this.props.trip.startDate)}-${this.formatDate(this.props.trip.endDate)}`}</h3>
-        //   <h3>Time: {`${this.props.trip.startTime} - ${this.props.trip.endTime}`}</h3>
-        //   <h3> Cost: ${this.props.trip.cost}</h3>
-        //   <h3> Description:</h3>
-        //   <p className="description" dangerouslySetInnerHTML={{ __html: marked(this.props.trip.description || '') }} />
-        //   {this.getMemberList()}
-        //   {this.getGearRequests()}
-        //   {this.getTrippeeGear()}
-        //   {this.appropriateButton()}
-        // </div>
+        <div>
+          <h1>Loading</h1>
+          <img src="/src/img/loading-gif.gif" alt="loading-gif" />
+        </div>
       );
     }
   }
 }
-
 
 // connects particular parts of redux state to this components props
 const mapStateToProps = state => (
   {
     trip: state.trips.trip,
     isUserOnTrip: state.trips.isUserOnTrip,
+    isLeaderOnTrip: state.trips.isLeaderOnTrip,
     user: state.user,
   }
 );
 
 export default withRouter(connect(mapStateToProps, {
-  joinTrip, addToPending, fetchTrip, leaveTrip, deleteTrip, isOnTrip, emailTrip,
+  fetchTrip, joinTrip, moveToPending, deleteTrip, addToPending, editUserGear, leaveTrip, appError,
 })(TripDetails));
