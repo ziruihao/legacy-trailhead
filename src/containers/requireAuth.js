69ee205c85f31befd377c9a85dbe1ba2c6427128
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-05 11:50:03 -0400

    commented our CAS process in RequireAuth for now

--- src/containers/requireAuth.js
+++ src/containers/requireAuth.js
@@ -2,38 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { signIn, appError, updateRestrictedPath, getUser } from '../actions';
+import Homepage from '../components/homepage';
 
 export default function (ComposedComponent, switchMode) {
   class RequireAuth extends Component {
-    componentWillMount() {
-      if (!this.props.authenticated) {
-        this.props.updateRestrictedPath(`${this.props.location.pathname}`);
-        // this.props.appError('Please sign in or sign up to view that page');
-        // this.props.history.push('/');
-        this.props.signIn();
-      }
-      this.props.getUser()
-        .then(() => {
-          // const { user } = this.props;
-          // console.log(user);
-          if (!this.props.user.hasCompleteProfile) {
-            console.log('here');
-            this.props.history.push('/user');
-          }
-        });
-    }
+    // componentWillMount() {
+    //   if (!this.props.authenticated) {
+    //     this.props.updateRestrictedPath(`${this.props.location.pathname}`);
+    //     // this.props.appError('Please sign in or sign up to view that page');
+    //     // this.props.history.push('/');
+    //     this.props.signIn();
+    //   }
+    //   this.props.getUser()
+    //     .then(() => {
+    //       // const { user } = this.props;
+    //       // console.log(user);
+    //       if (!this.props.user.hasCompleteProfile) {
+    //         console.log('here');
+    //         this.props.history.push('/user');
+    //       }
+    //     });
+    // }
 
-    isInfoEmpty = (string) => {
-      return !string || string.length === 0 || !string.toString().trim();
-    }
+    // isInfoEmpty = (string) => {
+    //   return !string || string.length === 0 || !string.toString().trim();
+    // }
 
     render() {
       return (
         <div>
-          <ComposedComponent
-            switchMode={switchMode ? true : undefined}
-            {...this.props}
-          />
+          {this.props.authenticated
+            ? (
+              <ComposedComponent
+                switchMode={switchMode ? true : undefined}
+                {...this.props}
+              />
+            )
+            : <Homepage />}
         </div>
       );
     }
