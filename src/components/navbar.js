a284c4d5162d057c52415e1dc45aac03e7fe70f8
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-06 23:36:44 -0400

    removed MyTrips option for OPO navigation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -21,10 +21,6 @@ class NavBar extends Component {
 
 
   render() {
-    if (this.props.errorMessage !== '') {
-      setTimeout(() => { this.props.clearError(); }, 5000);
-    }
-
     if (!this.props.authenticated) {
       return (
         <div>
@@ -32,49 +28,47 @@ class NavBar extends Component {
           {this.props.errorMessage === '' ? <div className="error" /> : <div className="alert alert-danger error">{this.props.errorMessage}</div>}
         </div>
       );
-    }
-
-    let dashboard;
-    if (this.props.role === 'OPO') {
-      dashboard = (
-        <NavDropdown onClick={this.goToDashboard} title="Dashboard" id="collapsible-nav-dropdown" className="dropdown-toggle drop-hover">
-          <NavDropdown.Item className="dropdown-hover" href="/opo-trips">Trip Approvals</NavDropdown.Item>
-          <NavDropdown.Item className="dropdown-hover" href="/vehicle-requests">Vehicle Requests</NavDropdown.Item>
-          <NavDropdown.Item className="dropdown-hover" href="/leader_approvals">Profile Approvals</NavDropdown.Item>
-          <NavDropdown.Item className="dropdown-hover" href="/mytrips">My Trips</NavDropdown.Item>
-          <NavDropdown.Divider className="dropdown-hover" />
-          <NavDropdown.Item className="dropdown-hover" href="/opo-dashboard">Back to Dashboard</NavDropdown.Item>
-        </NavDropdown>
-      );
     } else {
-      dashboard = (
-        <Nav.Link href="/mytrips">Dashboard</Nav.Link>
+      let dashboard;
+      if (this.props.user && this.props.user.role === 'OPO') {
+        dashboard = (
+          <NavDropdown onClick={this.goToDashboard} title="Dashboard" id="collapsible-nav-dropdown" className="dropdown-toggle drop-hover">
+            <NavDropdown.Item className="dropdown-hover" href="/opo-trips">Trip Approvals</NavDropdown.Item>
+            <NavDropdown.Item className="dropdown-hover" href="/vehicle-requests">Vehicle Requests</NavDropdown.Item>
+            <NavDropdown.Item className="dropdown-hover" href="/leader_approvals">Profile Approvals</NavDropdown.Item>
+            <NavDropdown.Divider className="dropdown-hover" />
+            <NavDropdown.Item className="dropdown-hover" href="/opo-dashboard">Back to Dashboard</NavDropdown.Item>
+          </NavDropdown>
+        );
+      } else {
+        dashboard = (
+          <Nav.Link href="/mytrips">Dashboard</Nav.Link>
+        );
+      }
+      return (
+        <div>
+          <Navbar collapseOnSelect fixed="top" expand="md" className="navbar-style">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav>
+                {dashboard}
+                <Nav.Link href="/alltrips">All Trips</Nav.Link>
+                <Nav.Link href="/user">Profile</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          {this.props.errorMessage === '' ? <div className="error" /> : <div className="alert alert-danger error">{this.props.errorMessage}</div>}
+        </div>
       );
     }
-
-
-    return (
-      <div>
-        <Navbar collapseOnSelect fixed="top" expand="md" className="navbar-style">
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav>
-              {dashboard}
-              <Nav.Link href="/alltrips">All Trips</Nav.Link>
-              <Nav.Link href="/user">Profile</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        {this.props.errorMessage === '' ? <div className="error" /> : <div className="alert alert-danger error">{this.props.errorMessage}</div>}
-      </div>
-    );
   }
 }
+
 const mapStateToProps = (state) => {
   return {
     authenticated: state.auth.authenticated,
     errorMessage: state.error.errorMessage,
-    role: state.user.role,
+    user: state.user.user,
   };
 };
 
