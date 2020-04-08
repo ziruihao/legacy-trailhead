7ed5274e2a1929d5f9c03b05598ecc3326fa0927
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-08 11:05:21 -0400

    evicted bootstrap, using custom styling for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter, NavLink } from 'react-router-dom';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -32,31 +32,28 @@ class NavBar extends Component {
       let dashboard;
       if (this.props.user && this.props.user.role === 'OPO') {
         dashboard = (
-          <NavDropdown onClick={this.goToDashboard} title="Dashboard" id="collapsible-nav-dropdown" className="dropdown-toggle drop-hover">
-            <NavDropdown.Item className="dropdown-hover" href="/opo-trips">Trip Approvals</NavDropdown.Item>
-            <NavDropdown.Item className="dropdown-hover" href="/vehicle-requests">Vehicle Requests</NavDropdown.Item>
-            <NavDropdown.Item className="dropdown-hover" href="/leader_approvals">Profile Approvals</NavDropdown.Item>
-            <NavDropdown.Divider className="dropdown-hover" />
-            <NavDropdown.Item className="dropdown-hover" href="/opo-dashboard">Back to Dashboard</NavDropdown.Item>
-          </NavDropdown>
+          <NavLink className="nav-link" to="/opo-dashboard">Dashboard</NavLink>
         );
       } else {
         dashboard = (
-          <Nav.Link href="/mytrips">Dashboard</Nav.Link>
+          <NavLink className="nav-link" to="/my-trips">Dashboard</NavLink>
         );
       }
       return (
-        <div>
-          <Navbar collapseOnSelect fixed="top" expand="md" className="navbar-style">
+        <div className="nav-bar">
+          {dashboard}
+          <NavLink className="nav-link" to="/all-trips">All Trips</NavLink>
+          <NavLink className="nav-link" to="/user">Profile</NavLink>
+          {/* <Navbar collapseOnSelect fixed="top" expand="md" className="navbar-style">
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav>
                 {dashboard}
-                <Nav.Link href="/alltrips">All Trips</Nav.Link>
-                <Nav.Link href="/user">Profile</Nav.Link>
+                <NavLink to="/all-trips">All Trips</NavLink>
+                <NavLink to="/user">Profile</NavLink>
               </Nav>
             </Navbar.Collapse>
-          </Navbar>
+          </Navbar> */}
           {this.props.errorMessage === '' ? <div className="error" /> : <div className="alert alert-danger error">{this.props.errorMessage}</div>}
         </div>
       );
