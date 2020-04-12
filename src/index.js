d0795e5ac1eec036e2c943100c996fa0213369d6
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-12 16:54:38 -0400

    refactored root app component

--- src/index.js
+++ src/index.js
@@ -3,27 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import axios from 'axios';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Switch } from 'react-router';
-import reducers from './reducers';
 
-import Dashboard from './containers/dashboard';
-import AllTrips from './components/allTrips';
-import CreateTrip from './components/createtrip';
-import MyTrips from './components/myTrips';
-import VehicleRequest from './components/vehiclerequest';
-import ProfilePage from './components/profilepage';
-import TripDetails from './components/tripdetails';
-import OpoApprovals from './components/opoStuff';
-import NavBar from './components/navbar';
-import OpoTrips from './components/opotrips';
-import OpoVehicleRequests from './components/opoVehicleRequests';
-import OpoVehicleRequest from './components/opoVehicleRequest';
-import OPODashboard from './components/opoDashboard';
-import requireAuth from './containers/requireAuth';
-import VehicleCalendar from './components/vehiclecalendar';
-import { ActionTypes, getUser, getClubs, getVehicles } from './actions';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
+import { ActionTypes } from './actions';
+
+import App from './containers/app';
+
 import './styles/base.scss';
 import './styles/homepage-style.scss';
 
@@ -38,11 +24,8 @@ const token = localStorage.getItem('token');
 const ROOT_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-planner.herokuapp.com/api';
 if (token) {
   axios.get(`${ROOT_URL}/user`, { headers: { authorization: token } })
-    .then((response) => {
+    .then(() => {
       store.dispatch({ type: ActionTypes.AUTH_USER });
-      store.dispatch({ type: ActionTypes.UPDATE_USER, payload: response.data.user });
-      store.dispatch(getClubs());
-      store.dispatch(getVehicles());
     })
     .catch(() => {
       localStorage.clear();
@@ -50,39 +33,6 @@ if (token) {
     });
 }
 
-const FallBack = (props) => {
-  return <div>URL Not Found</div>;
-};
-
-
-const App = (props) => {
-  return (
-    <Router>
-      <NavBar />
-      <div id="theBody">
-        <Switch>
-          <Route exact path="/" component={requireAuth(Dashboard)} />
-          <Route path="/user" component={requireAuth(ProfilePage)} />
-          <Route path="/all-trips" component={requireAuth(AllTrips)} />
-          <Route path="/vehicle-request/:vehicleReqId" component={requireAuth(VehicleRequest, 'viewMode')} />
-          <Route path="/vehicle-request" component={requireAuth(VehicleRequest)} />
-          <Route path="/trip/:tripID" component={requireAuth(TripDetails)} />
-          <Route path="/createtrip" component={requireAuth(CreateTrip)} />
-          <Route path="/my-trips" component={requireAuth(MyTrips)} />
-          <Route path="/edittrip/:tripID" component={requireAuth(CreateTrip, 'editMode')} />
-          <Route path="/opo-trips" component={requireAuth(OpoTrips)} />
-          <Route path="/vehicle-requests" component={requireAuth(OpoVehicleRequests)} />
-          <Route path="/opo-vehicle-request/:vehicleReqId" component={requireAuth(OpoVehicleRequest)} />
-          <Route path="/opo-dashboard" component={requireAuth(OPODashboard)} />
-          <Route path="/leader-approvals" component={requireAuth(OpoApprovals)} />
-          <Route path="/vehicle-calendar" component={requireAuth(VehicleCalendar)} />
-          <Route component={FallBack} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
-// component={requireAuth() => <CreateTrip isEditMode={true} />}
 ReactDOM.render(
   <Provider store={store}>
     <App />
