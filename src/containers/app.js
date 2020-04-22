b93e6522d6b8fdbb34f1207b35bfabd1c4216b45
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-22 16:33:50 -0400

    final fix to auth

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Switch } from 'react-router';
-
+import axios from 'axios';
 import { connect } from 'react-redux';
 import Dashboard from './dashboard';
 import AllTrips from '../components/allTrips';
@@ -21,6 +21,8 @@ import VehicleCalendar from '../components/vehiclecalendar';
 
 import { getUser, getClubs, getVehicles } from '../actions';
 
+const ROOT_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-planner.herokuapp.com/api';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -30,9 +32,11 @@ class App extends React.Component {
   }
 
   componentWillMount() {
-    if (this.props.authenaticated) {
+    this.verifyToken().then(() => {
       this.loadData();
-    } else { this.setState({ loaded: true }); }
+    }).catch(() => {
+      this.setState({ loaded: true });
+    });
   }
 
   loadData = () => {
@@ -45,6 +49,26 @@ class App extends React.Component {
           });
         });
       }).catch(error => reject(error));
+    });
+  }
+
+  verifyToken = () => {
+    return new Promise((resolve, reject) => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        console.log('there is a token');
+        axios.get(`${ROOT_URL}/user`, { headers: { authorization: token } })
+          .then(() => {
+            console.log('token works');
+            resolve();
+          })
+          .catch(() => {
+            console.log('token doesnt work');
+            localStorage.clear();
+            reject();
+            // store.dispatch({ type: ActionTypes.DEAUTH_USER });
+          });
+      } else { reject(); }
     });
   }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import axios from 'axios';
-
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
-import { ActionTypes } from './actions';
 
 import App from './containers/app';
 
@@ -19,21 +16,6 @@ const store = createStore(reducers, {}, compose(
   applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
 ));
-
-const token = localStorage.getItem('token');
-const ROOT_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-planner.herokuapp.com/api';
-if (token) {
-  console.log('there is a token');
-  axios.get(`${ROOT_URL}/user`, { headers: { authorization: token } })
-    .then(() => {
-      store.dispatch({ type: ActionTypes.AUTH_USER });
-    })
-    .catch(() => {
-      console.log('token doesnt work');
-      localStorage.clear();
-      store.dispatch({ type: ActionTypes.DEAUTH_USER });
-    });
-}
 
 ReactDOM.render(
   <Provider store={store}>
