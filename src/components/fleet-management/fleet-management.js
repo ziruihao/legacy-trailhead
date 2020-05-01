aff1ec51151d6745514da2324efd1053957384db
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-01 15:21:44 -0400

    added card styling to base

--- src/components/fleet-management/fleet-management.js
+++ src/components/fleet-management/fleet-management.js
@@ -1,58 +1,69 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import Table from 'react-bootstrap/Table';
+import { Table, Form, Dropdown, DropdownButton, DropdownToggle } from 'react-bootstrap';
 import { fetchVehicleRequests } from '../../actions';
-import Toggle from '../toggle/toggle';
-import loadingGif from '../../img/loading-gif.gif';
 import './fleet-management.scss';
 
 class FleetManagement extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      newVehicleNameField: null,
     };
+  }
+
+  changeNewVehicleNameField = (event) => {
+    this.setState({ newVehicleNameField: event.target.value });
   }
 
   render() {
     return (
-      <div id="fleet-management">
-        <div id="fleet">
-          <div className="h1">DOC Vehicles</div>
-          <Table responsive="lg" hover>
-            <thead>
-              <tr>
-                <th>Vehicle Name</th>
-                <th>Type</th>
-                <th>Booked by</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.vehicles.map(vehicle => (
-                <tr key={vehicle._id} onClick={() => this.onRowClick(vehicle._id)}>
-                  <td>{vehicle.name}</td>
-                  <td>{vehicle.type}</td>
-                  <td>To implement</td>
-                  <td className="fleet-delete-button-area"><div id="fleet-delete-button" className="doc-button">Delete</div></td>
+      <div id="fleet-management-page" className="center-view">
+        <div className="h1">DOC Vehicles</div>
+        <div id="fleet-management">
+          <div id="fleet">
+            <Table className="doc-table" responsive="lg" hover>
+              <thead>
+                <tr>
+                  <th>Vehicle Name</th>
+                  <th>Type</th>
+                  <th id="fleet-management-book-col">Booked by</th>
+                  <th>Action</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
-        </div>
-        <div id="management">
-          <div className="h1">Add New Vehicle</div>
-          <div id="fleet-management-input">
-            <p>Manage</p>
-            <input className={`form-control field top-create-trip ${false ? 'create-trip-error' : ''}`}
-                // onChange={props.onFieldChange}
-              name="title"
-              placeholder="e.g. Weekend Mt. Moosilauke Hike!"
-              value="test"
-            />
-            <div className="doc-button">Add</div>
+              </thead>
+              <tbody>
+                {this.props.vehicles.map(vehicle => (
+                  <tr key={vehicle._id} onClick={() => this.onRowClick(vehicle._id)}>
+                    <td>{vehicle.name}</td>
+                    <td>{vehicle.type}</td>
+                    <td id="fleet-management-book-col">To implement</td>
+                    <td className="fleet-delete-button-area"><div id="fleet-delete-button" className="doc-button">Delete</div></td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+          <div id="management">
+            <div className="doc-card">
+              <div className="doc-form">
+                <div className="h2">Add New Vehicle</div>
+                <div className="field-label">Vehicle name</div>
+                <input className={`field ${this.state.newVehicleNameField && this.state.newVehicleNameField !== '' ? 'create-trip-error' : ''}`}
+                  onChange={this.changeNewVehicleNameField}
+                  name="title"
+                  placeholder="Example van"
+                  value={this.state.newVehicleNameField}
+                />
+                <div className="field-label">Vehicle type</div>
+                <select className={`field ${this.state.newVehicleNameField && this.state.newVehicleNameField !== '' ? 'create-trip-error' : ''}`} />
+                <div className="doc-button">Add</div>
+              </div>
+            </div>
+
           </div>
         </div>
+
       </div>
     );
   }
