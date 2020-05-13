f614bad5134163a308888428625377ba0cc7d6be
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-13 13:42:05 -0400

    updated backend url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-planner.herokuapp.com/api';
+export const BACKEND_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-logistics-app.herokuapp.com/api';
 
 export const hi = 'hi';
