289575f61637282b3d901ddd34ccc5cb5a198f56
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-15 01:05:02 -0400

    more profile styling

--- src/components/profile-card/profile-card.js
+++ src/components/profile-card/profile-card.js
@@ -2,7 +2,8 @@ import React from 'react';
 import '../profile-page/profile-page.scss';
 import leaderBadge from './leader-badge.svg';
 import leaderPendingBadge from './leader-pending-badge.svg';
-import editIcon from '../../img/editButton.svg';
+import editIcon from './edit-profile.svg';
+import saveIcon from './save-profile.svg';
 
 const TRAILER_CONSTANT = 'TRAILER';
 
@@ -58,7 +59,7 @@ const ProfileCard = (props) => {
               </div>
             )
             : null
-        }
+          }
           {props.user.completedProfile
             ? (
               <div id="profile-card-name">
@@ -84,7 +85,7 @@ const ProfileCard = (props) => {
           }
           {props.asProfilePage
             ? (
-              <input id="profile-card-edit-button" type="image" src={editIcon} alt="edit button" onClick={props.startEditing} />
+              <input className="profile-card-edit-toggle" type="image" src={editIcon} alt="edit button" onClick={props.startEditing} />
             )
             : null}
         </div>
@@ -179,176 +180,172 @@ const ProfileCard = (props) => {
   } else {
     return (
       <div id="profile-card">
-        {props.user.name
-          ? (
-            <div className="profile-pic-container">
-              <div className="profile-pic">
+        <div id="profile-card-picture-and-name">
+          {props.user.name
+            ? (
+              <div id="profile-card-picture">
                 <span className="user-initials">{getUserInitials(props.user.name)}</span>
               </div>
+            )
+            : null
+          }
+          <div id="profile-card-name">
+            <div className="h1">
+              <input
+                type="text"
+                name="name"
+                onChange={props.onFieldChange}
+                className={`my-form-control name-input ${props.errorFields.name ? 'vrf-error' : ''}`}
+                value={props.name}
+                placeholder="Name"
+              />
             </div>
-          )
-          : null
-        }
-        <div className="profile-card-body">
-          <div className="profile-card-header">
-            <div className="name-and-email">
-              <div className="card-name">
-                <input
-                  type="text"
-                  name="name"
-                  onChange={props.onFieldChange}
-                  className={`my-form-control name-input ${props.errorFields.name ? 'vrf-error' : ''}`}
-                  value={props.name}
-                  placeholder="Name"
-                />
-              </div>
-              <div className="card-email">
-                <input
-                  type="text"
-                  name="email"
-                  maxLength="50"
-                  onChange={props.onFieldChange}
-                  className={`my-form-control ${props.errorFields.email ? 'vrf-error' : ''}`}
-                  value={props.email}
-                  placeholder="Dartmouth email"
-                />
-              </div>
-            </div>
-            <div className="button-place">
-              <span className="logout-button" onClick={props.updateUserInfo} role="button" tabIndex={0}>Save</span>
+            <div className="p1">
+              <input
+                type="text"
+                name="email"
+                maxLength="50"
+                onChange={props.onFieldChange}
+                className={`my-form-control ${props.errorFields.email ? 'vrf-error' : ''}`}
+                value={props.email}
+                placeholder="Dartmouth email"
+              />
             </div>
           </div>
-          <div className="profile-card-info">
-            <div className="profile-card-row">
-              <span className="card-headings h3">
-                Pronouns
-              </span>
-              <span className="card-info p1">
-                <input
-                  className={`my-form-control ${props.errorFields.pronoun ? 'vrf-error' : ''}`}
-                  type="text"
-                  name="pronoun"
-                  maxLength="50"
-                  onChange={props.onFieldChange}
-                  value={props.pronoun}
-                />
-              </span>
-            </div>
-            <hr />
-            <div className="profile-card-row">
-              <span className="card-headings h3">
-                DASH
-              </span>
-              <span className="card-info p1">
-                <input
+          <input className="profile-card-edit-toggle" type="image" src={saveIcon} alt="save button" onClick={props.updateUserInfo} />
+        </div>
+        <hr />
+        <div id="profile-card-info">
+          <div className="profile-card-row">
+            <span className="card-headings h3">
+              Pronouns
+            </span>
+            <span className="card-info p1">
+              <input
+                className={`my-form-control ${props.errorFields.pronoun ? 'vrf-error' : ''}`}
+                type="text"
+                name="pronoun"
+                maxLength="50"
+                onChange={props.onFieldChange}
+                value={props.pronoun}
+              />
+            </span>
+          </div>
+          <hr />
+          <div className="profile-card-row">
+            <span className="card-headings h3">
+              DASH
+            </span>
+            <span className="card-info p1">
+              <input
                   // type="number"
-                  id="dash_number"
-                  name="dash_number"
-                  maxLength="50"
-                  onChange={props.onFieldChange}
-                  className={`my-form-control ${props.errorFields.dash_number ? 'vrf-error' : ''}`}
-                  value={props.dash_number}
-                />
-              </span>
-            </div>
-            <hr />
-            <div className="profile-card-row">
-              <span className="card-headings h3">
-                Clothing Size
-              </span>
-              <span className="card-info p1">
-                {props.getClotheForm()}
-              </span>
-            </div>
-            <hr />
-            <div className="profile-card-row">
-              <span className="card-headings h3">
-                Shoe Size
-              </span>
-              <span className="card-info p1">
-                {props.getShoeGender()}
-                <input
+                id="dash_number"
+                name="dash_number"
+                maxLength="50"
+                onChange={props.onFieldChange}
+                className={`my-form-control ${props.errorFields.dash_number ? 'vrf-error' : ''}`}
+                value={props.dash_number}
+              />
+            </span>
+          </div>
+          <hr />
+          <div className="profile-card-row">
+            <span className="card-headings h3">
+              Clothing Size
+            </span>
+            <span className="card-info p1">
+              {props.getClotheForm()}
+            </span>
+          </div>
+          <hr />
+          <div className="profile-card-row">
+            <span className="card-headings h3">
+              Shoe Size
+            </span>
+            <span className="card-info p1">
+              {props.getShoeGender()}
+              <input
                   // type="number"
-                  name="shoe_size"
-                  step="0.5"
-                  onChange={props.onFieldChange}
-                  className={`my-form-control ${props.errorFields.shoe_size ? 'vrf-error' : ''}`}
-                  value={props.shoe_size}
-                  placeholder="e.g. 9.5"
-                />
-              </span>
-            </div>
-            <hr />
-            <div className="profile-card-row">
-              <span className="card-headings h3">
-                Height
-              </span>
-              <span className="card-info p1">
-                <input
-                  type="text"
-                  name="height"
-                  onChange={props.onFieldChange}
-                  className={`my-form-control ${props.errorFields.height ? 'vrf-error' : ''}`}
-                  value={props.height}
-                  placeholder={'e.g. 5\'2"'}
-                />
-              </span>
-            </div>
-            <hr />
-            <div className="profile-card-row">
-              <span className="card-headings h3">
-                Allergies/Dietary Restrictions
-              </span>
-              <span className="card-info p1">
-                <input
-                  className={`my-form-control ${props.errorFields.allergies_dietary_restrictions ? 'vrf-error' : ''}`}
-                  type="text"
-                  name="allergies_dietary_restrictions"
-                  maxLength="50"
-                  onChange={props.onFieldChange}
-                  value={props.allergies_dietary_restrictions}
-                />
-              </span>
-            </div>
-            <hr />
-            <div className="profile-card-row">
-              <span className="card-headings h3">
-                Relevant Medical Conditions
-              </span>
-              <span className="card-info p1 extra-info">
-                <input
-                  type="text"
-                  name="medical"
-                  maxLength="50"
-                  onChange={props.onFieldChange}
-                  className={`my-form-control ${props.errorFields.medical ? 'vrf-error' : ''}`}
-                  value={props.medical}
-                />
-                <span className="extra-info-message">This will only be visible to your trip leaders and OPO staff</span>
-              </span>
-            </div>
-            <hr />
-            <div className="profile-card-row">
-              <span className="card-headings h3 extra-info">
-                Driver Certification(s)
-                {props.displayCertificationFeedback()}
-              </span>
-              <span className="card-info p1 extra-info">
-                {props.getCertificationsForm()}
-                <span className="extra-info-message">Please select your highest level of driver certification</span>
-              </span>
-            </div>
-            <hr />
-            <div className="profile-card-row">
-              <span className="card-headings h3 extra-info">
-                DOC Leadership
-                {props.displayLeaderFeedback()}
-              </span>
-              <span className="card-info p1">
-                {props.getClubForm()}
-              </span>
-            </div>
-            {/* {props.user.role !== 'OPO' ? <hr /> : null}
+                name="shoe_size"
+                step="0.5"
+                onChange={props.onFieldChange}
+                className={`my-form-control ${props.errorFields.shoe_size ? 'vrf-error' : ''}`}
+                value={props.shoe_size}
+                placeholder="e.g. 9.5"
+              />
+            </span>
+          </div>
+          <hr />
+          <div className="profile-card-row">
+            <span className="card-headings h3">
+              Height
+            </span>
+            <span className="card-info p1">
+              <input
+                type="text"
+                name="height"
+                onChange={props.onFieldChange}
+                className={`my-form-control ${props.errorFields.height ? 'vrf-error' : ''}`}
+                value={props.height}
+                placeholder={'e.g. 5\'2"'}
+              />
+            </span>
+          </div>
+          <hr />
+          <div className="profile-card-row">
+            <span className="card-headings h3">
+              Allergies/Dietary Restrictions
+            </span>
+            <span className="card-info p1">
+              <input
+                className={`my-form-control ${props.errorFields.allergies_dietary_restrictions ? 'vrf-error' : ''}`}
+                type="text"
+                name="allergies_dietary_restrictions"
+                maxLength="50"
+                onChange={props.onFieldChange}
+                value={props.allergies_dietary_restrictions}
+              />
+            </span>
+          </div>
+          <hr />
+          <div className="profile-card-row">
+            <span className="card-headings h3">
+              Relevant Medical Conditions
+            </span>
+            <span className="card-info p1 extra-info">
+              <input
+                type="text"
+                name="medical"
+                maxLength="50"
+                onChange={props.onFieldChange}
+                className={`my-form-control ${props.errorFields.medical ? 'vrf-error' : ''}`}
+                value={props.medical}
+              />
+              <span className="extra-info-message">This will only be visible to your trip leaders and OPO staff</span>
+            </span>
+          </div>
+          <hr />
+          <div className="profile-card-row">
+            <span className="card-headings h3 extra-info">
+              Driver Certification(s)
+              {props.displayCertificationFeedback()}
+            </span>
+            <span className="card-info p1 extra-info">
+              {props.getCertificationsForm()}
+              <span className="extra-info-message">Please select your highest level of driver certification</span>
+            </span>
+          </div>
+          <hr />
+          <div className="profile-card-row">
+            <span className="card-headings h3 extra-info">
+              DOC Leadership
+              {props.displayLeaderFeedback()}
+            </span>
+            <span className="card-info p1">
+              {props.getClubForm()}
+            </span>
+          </div>
+          {/* {props.user.role !== 'OPO' ? <hr /> : null}
             {props.user.role !== 'OPO'
               ? (
                 <div className="profile-card-row">
@@ -361,7 +358,6 @@ const ProfileCard = (props) => {
                   </span>
                 </div>
               ) : null} */}
-          </div>
         </div>
       </div>
     );
