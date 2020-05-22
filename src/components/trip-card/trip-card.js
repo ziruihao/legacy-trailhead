fdecfe3ccf00f59191acf77da3ea601f37c26966
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-22 14:35:43 -0400

    built trip-card component

--- src/components/trip-card/trip-card.js
+++ src/components/trip-card/trip-card.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import leaderBadge from './leader-badge.svg';
+import ReactToolTip from 'react-tooltip';
+import Badge from '../badge';
 import * as constants from '../../constants';
+import ledyard from './decals/ledyard.png';
+import mountain from './decals/dmc.png';
+import doc from './decals/doc.png';
+import cnt from './decals/cnt.png';
+import wiw from './decals/wiw.png';
+import wood from './decals/woodsmen.png';
+import surf from './decals/surf.png';
+import dmbc from './decals/dmbc.png';
+import wsc from './decals/wsc.png';
 
 class TripCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       userIsLeader: false,
+      status: 'approved',
+      reasons: [],
     };
   }
 
@@ -17,17 +29,68 @@ class TripCard extends React.Component {
         this.setState({ userIsLeader: true });
       }
     }));
+    // calculates the final status of the trip
+    const statuses = [{ name: 'Group gear requests', state: this.props.trip.gearStatus }, { name: 'Trippee gear requests', state: this.props.trip.trippeeGearStatus }, { name: 'P-Card request', state: this.props.trip.pcardStatus }, { name: 'Vehicle request', state: this.props.trip.vehicleStatus }];
+    statuses.forEach((status) => {
+      switch (status.state) {
+        case 'pending':
+          this.setState((prevState) => {
+            prevState.reasons.push(`${status.name} is still pending`);
+            if (prevState.status === 'approved') return { reasons: prevState.reasons, status: 'pending' };
+            else return { reasons: prevState.reasons };
+          });
+          break;
+        case 'denied':
+          this.setState((prevState) => {
+            prevState.reasons.push(`${status.name} was denied`);
+            return { reasons: prevState.reasons, status: 'warning' };
+          });
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  renderDecal = (clubName) => {
+    switch (clubName) {
+      case 'Cabin and Trail':
+        return <img className="trip-card-decal" src={cnt} alt="" />;
+      case 'Women in the Wilderness':
+        return <img className="trip-card-decal" src={wiw} alt="" />;
+      case 'Surf Club':
+        return <img className="trip-card-decal" src={surf} alt="" />;
+      case 'Mountain Biking':
+        return <img className="trip-card-decal" src={dmbc} alt="" />;
+      case 'Winter Sports':
+        return <img className="trip-card-decal" src={wsc} alt="" />;
+      case 'Woodsmen':
+        return <img className="trip-card-decal" src={wood} alt="" />;
+      case 'Mountaineering':
+        return <img className="trip-card-decal" src={mountain} alt="" />;
+      case 'Ledyard':
+        return <img className="trip-card-decal" src={ledyard} alt="" />;
+      default:
+        return <img className="trip-card-decal" src={doc} alt="" />;
+    }
   }
 
   render() {
     return (
-      <div className="card trip-card margins">
-
-        <div className="card-body" id={constants.clubDictionary(this.props.trip.club.name)}>
+      <div className="trip-card margins">
+        <div className="trip-card-badge-holder" data-tip data-for={this.props.trip._id}>
+          <Badge type={this.state.status} />
+        </div>
+        <ReactToolTip id={this.props.trip._id} place="bottom">
+          Reasons: {this.state.reasons}
+        </ReactToolTip>
+        {this.renderDecal(this.props.trip.club.name)}
+        <div className="trip-card-body">
           <div className="label-text">TRIP #{this.props.trip.number}</div>
-          <div className="h1">{this.state.userIsLeader ? '[L]' : null} {this.props.trip.title}</div>
-          <p className="card-text">{constants.formatDate(this.props.trip.startDate)} - {constants.formatDate(this.props.trip.endDate)}</p>
-          <p className="card-club">{this.props.trip.club ? this.props.trip.club.name : ''}</p>
+          <div className="doc-h1">{this.state.userIsLeader ? '[L]' : null} {this.props.trip.title}</div>
+          <div className="p1 trip-card-date">{constants.formatDate(this.props.trip.startDate)} - {constants.formatDate(this.props.trip.endDate)}</div>
+          <div className="p2 trip-card-club">{this.props.trip.club ? this.props.trip.club.name : ''}</div>
+          <div className="p2">{this.props.trip.description}</div>
         </div>
       </div>
     );
