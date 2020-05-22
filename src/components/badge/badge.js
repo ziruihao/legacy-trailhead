4fbbd1c09450369a7fbefe71da321592543aa69f
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-22 14:34:22 -0400

    refactored badge

--- src/components/badge/badge.js
+++ src/components/badge/badge.js
@@ -4,19 +4,20 @@ import denied from './denied.svg';
 import leader from './leader.svg';
 import pending from './pending.svg';
 import warning from './warning.svg';
+import './badge.scss';
 
-export const Badge = (props) => {
+const Badge = (props) => {
   switch (props.type) {
     case 'approved':
       return (<img className="doc-badge" alt="approved badge" src={approved} />);
     case 'denied':
-      return (<img className="doc-badge" alt="approved badge" src={denied} />);
+      return (<img className="doc-badge" alt="denied badge" src={denied} />);
     case 'leader':
-      return (<img className="doc-badge" alt="approved badge" src={leader} />);
+      return (<img className="doc-badge" alt="leader badge" src={leader} />);
     case 'pending':
-      return (<img className="doc-badge" alt="approved badge" src={pending} />);
+      return (<img className="doc-badge" alt="pending badge" src={pending} />);
     case 'warning':
-      return (<img className="doc-badge" alt="approved badge" src={warning} />);
+      return (<img className="doc-badge" alt="warning badge" src={warning} />);
     default:
       return null;
   }
