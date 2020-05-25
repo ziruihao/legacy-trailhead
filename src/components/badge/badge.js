dbc318b53ac72762ce2550d810bee8863d966a0d
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-24 21:06:07 -0400

    added ReactToolTip props to badge component

--- src/components/badge/badge.js
+++ src/components/badge/badge.js
@@ -9,15 +9,15 @@ import './badge.scss';
 const Badge = (props) => {
   switch (props.type) {
     case 'approved':
-      return (<img className="doc-badge" alt="approved badge" src={approved} />);
+      return (<img className="doc-badge" alt="approved badge" src={approved} data-tip={props.dataTip} data-for={props.dataFor} />);
     case 'denied':
-      return (<img className="doc-badge" alt="denied badge" src={denied} />);
+      return (<img className="doc-badge" alt="denied badge" src={denied} data-tip={props.dataTip} data-for={props.dataFor} />);
     case 'leader':
-      return (<img className="doc-badge" alt="leader badge" src={leader} />);
+      return (<img className="doc-badge" alt="leader badge" src={leader} data-tip={props.dataTip} data-for={props.dataFor} />);
     case 'pending':
-      return (<img className="doc-badge" alt="pending badge" src={pending} />);
+      return (<img className="doc-badge" alt="pending badge" src={pending} data-tip={props.dataTip} data-for={props.dataFor} />);
     case 'warning':
-      return (<img className="doc-badge" alt="warning badge" src={warning} />);
+      return (<img className="doc-badge" alt="warning badge" src={warning} data-tip={props.dataTip} data-for={props.dataFor} />);
     default:
       return null;
   }
