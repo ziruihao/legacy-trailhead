779d9d6e3e85a1bb213802e4c13de3ea15492fd6
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-26 19:38:24 -0400

    fixing array key issue

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -5,12 +5,12 @@ const Sidebar = (props) => {
   return (
     <div id="sidebar">
       {props.sections.map(section => (
-        <div className="sidebar-section">
+        <div className="sidebar-section" key={section.title}>
           <div className="sidebar-section-title doc-h2">
             {section.title}
           </div>
           {section.steps.map(step => (
-            <div className="sidebar-section-row">
+            <div className="sidebar-section-row" key={step.number}>
               <div className={props.currentStep === step.number ? 'sidebar-highlight' : ''} />
               <div className={`${props.currentStep === step.number ? 'sidebar-text-highlight' : ''} doc-h3`}>{step.text}</div>
             </div>
