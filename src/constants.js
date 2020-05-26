d6586af7cea0574bf96fbdc03ad655c77d2dc412
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-25 20:25:16 -0400

    refactored to utils

--- src/constants.js
+++ src/constants.js
@@ -3,26 +3,6 @@ export const ROOT_URL = process.env.NODE_ENV === 'development' ? 'http://localho
 
 export const green = '#0CA074';
 
-export const formatDate = (date) => {
-  const rawDate = new Date(date);
-  const dateString = rawDate.toUTCString();
-  return dateString.substring(0, 11);
-};
-
-export const formatTime = (time) => {
-  const splitTime = time.split(':');
-  splitTime.push(' AM');
-  const originalHour = splitTime[0];
-  splitTime[0] = originalHour % 12;
-  if (originalHour >= 12) {
-    splitTime[2] = ' PM';
-  }
-  if (splitTime[0] === 0) {
-    splitTime[0] = 12;
-  }
-  return `${splitTime[0]}:${splitTime[1]}${splitTime[2]}`;
-};
-
 export const calculateTripStatus = (trip) => {
   const statuses = [{ name: 'Group gear requests', state: trip.gearStatus }, { name: 'Trippee gear requests', state: trip.trippeeGearStatus }, { name: 'P-Card request', state: trip.pcardStatus }, { name: 'Vehicle request', state: trip.vehicleStatus }];
   let finalStatus = 'approved';
