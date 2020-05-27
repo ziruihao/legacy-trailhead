cf409f299aa58e5affc46d4cff1bf8377bba72ac
ziruihao <ziray.hao.22@dartmouth.edu>
2020-05-27 14:33:36 -0400

    url fix

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-logistics-app.herokuapp.com/api';
+export const BACKEND_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9090/api' : 'https://doc-dartmouth.herokuapp.com/api';
 export const ROOT_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://doc-dartmouth.surge.sh';
 
 export const green = '#0CA074';
