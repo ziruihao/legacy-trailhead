7846d0f042214cdda02879721250b82427ed188f
ziruihao <ziray.hao.22@dartmouth.edu>
2020-06-01 16:56:47 -0400

    refactored opo trip approval page

--- src/components/opo-approvals/trip/trip-approval.js
+++ src/components/opo-approvals/trip/trip-approval.js
@@ -8,15 +8,17 @@ import { GearRequest, BasicInfo, PCardRequest } from '../../opo-trip-info-pages'
 import OPOVehicleRequest from '../../opo-vehicle-request';
 import Badge from '../../badge';
 import Sidebar from '../../sidebar';
-import { reviewGearRequest, reviewTrippeeGearRequest, reviewPCardRequests, appError } from '../../../actions';
+import Loading from '../../doc-loading';
+import { fetchTrip, reviewGearRequest, reviewTrippeeGearRequest, reviewPCardRequests, appError } from '../../../actions';
 import '../../../styles/tripdetails_opo.scss';
 import '../../../styles/createtrip-style.scss';
 
 
-class OPOTripDetails extends Component {
+class OPOTripApproval extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loaded: false,
       step: 1,
       pcardAssigned: '',
       showModal: false,
@@ -40,6 +42,9 @@ class OPOTripDetails extends Component {
     }
     const isEditingPcard = trip.pcardStatus === 'pending';
     this.setState({ numOfPages, isEditingPcard });
+    this.props.fetchTrip(this.props.match.params.tripID).then(() => {
+      this.setState({ loaded: true });
+    });
   }
 
   setStep = (step) => {
@@ -204,111 +209,112 @@ class OPOTripDetails extends Component {
   }
 
   render() {
-    let page;
-    switch (this.state.step) {
-      case 1:
-        page = (
-          <BasicInfo
-            trip={this.props.trip}
-          />
-        );
-        break;
-      case 2:
-        page = (
-          <GearRequest
-            trip={this.props.trip}
-            reviewGroupGearRequest={this.reviewGroupGearRequest}
-            reviewTrippeeGearRequest={this.reviewTrippeeGearRequest}
-          />
-        );
-        break;
-      case 3:
-        page = (
-          <PCardRequest
-            trip={this.props.trip}
-            approve={this.approve}
-            deny={this.deny}
-            onFieldChange={this.onFieldChange}
-            pcardAssigned={this.state.pcardAssigned}
-            isEditingPcard={this.state.isEditingPcard}
-            reviewPcardRequest={this.reviewPcardRequest}
-            startEditingPcard={this.startEditingPcard}
-            cancelPcardUpdate={this.cancelPcardUpdate}
-          />
-        );
-        break;
-      case 4:
-        page = (
-          <OPOVehicleRequest
-            partOfTrip
-            vehicleReqId={this.props.trip.vehicleRequest._id}
-          />
-        );
-        break;
-      default:
-        page = (
-          <BasicInfo
-            trip={this.props.trip}
-          />
-        );
-        break;
-    }
-    return (
-      <Box>
-        <Sidebar
-          sections={
+    if (this.state.loaded) {
+      let page;
+      switch (this.state.step) {
+        case 1:
+          page = (
+            <BasicInfo
+              trip={this.props.trip}
+            />
+          );
+          break;
+        case 2:
+          page = (
+            <GearRequest
+              trip={this.props.trip}
+              reviewGroupGearRequest={this.reviewGroupGearRequest}
+              reviewTrippeeGearRequest={this.reviewTrippeeGearRequest}
+            />
+          );
+          break;
+        case 3:
+          page = (
+            <PCardRequest
+              trip={this.props.trip}
+              approve={this.approve}
+              deny={this.deny}
+              onFieldChange={this.onFieldChange}
+              pcardAssigned={this.state.pcardAssigned}
+              isEditingPcard={this.state.isEditingPcard}
+              reviewPcardRequest={this.reviewPcardRequest}
+              startEditingPcard={this.startEditingPcard}
+              cancelPcardUpdate={this.cancelPcardUpdate}
+            />
+          );
+          break;
+        case 4:
+          page = (
+            <OPOVehicleRequest
+              partOfTrip
+              vehicleReqId={this.props.trip.vehicleRequest._id}
+            />
+          );
+          break;
+        default:
+          page = (
+            <BasicInfo
+              trip={this.props.trip}
+            />
+          );
+          break;
+      }
+      return (
+        <Box dir="row">
+          <Sidebar
+            sections={
             [
               { title: 'Trip Overview', steps: [{ number: 1, text: 'Basic information' }, { number: 2, text: 'Attendies' }] },
               { title: 'Requests', steps: [{ number: 3, text: 'Gear' }, { number: 4, text: 'P-Card' }, { number: 5, text: 'Vehicles' }] },
             ]
           }
-          currentStep={this.state.currentStep}
-        />
-
-        <div className="ovr-req-content">
-          <div className="otd-req-content">
-            {page}
-          </div>
-          <div className="create-trip-bottom-buttons create-trips-top-margin">
-            <button disabled={this.state.step === 1} type="button" className="btn next-button" onClick={this.previousPage}>Previous</button>
-            <span className="cancel-link ovr-bottom-link ovr-contact-link" onClick={this.openModal} role="button" tabIndex={0}>Contact Trip Leader</span>
-            <button type="button" className="btn next-button" onClick={this.nextPage}>
-              {this.state.step === this.state.numOfPages ? 'Back to Trip Approvals' : 'Next'}
-            </button>
-          </div>
-        </div>
-        <Modal
-          centered
-          show={this.state.showModal}
-          onHide={this.closeModal}
-        >
-          <div className="trip-details-close-button">
-            <i className="material-icons close-button" onClick={this.closeModal} role="button" tabIndex={0}>close</i>
-          </div>
-          <Badge type="denied" />
-          {/* <img className="status-badge ovr-status-badge" src={this.badges.denied} alt="warning_badge" /> */}
-
-          <div className="cancel-content">
-            <p className="cancel-question">{`Contact ${this.props.trip.leaders[0].name}`}</p>
-            <p className="cancel-message">
-              Please email the requester if you need them to update a request. They can't update a request after it has been reviewed.
-            </p>
-            <div className="ovr-modal-button-container">
-              <input
-                ref={this.emailRef}
-                type="text"
-                className="ovr-requester-email"
-                defaultValue={this.props.trip.leaders[0].email}
-              />
+            currentStep={this.state.currentStep}
+          />
+          <Box dir="col" pad={100} expand>
+            <Box dir="col">
+              {page}
+            </Box>
+            <Stack size={100} />
+            <Box dir="row" justify="between" align="center">
+              <button disabled={this.state.step === 1} type="button" className="btn next-button" onClick={this.previousPage}>Previous</button>
+              <span className="cancel-link ovr-bottom-link ovr-contact-link" onClick={this.openModal} role="button" tabIndex={0}>Contact Trip Leader</span>
+              <button type="button" className="btn next-button" onClick={this.nextPage}>
+                {this.state.step === this.state.numOfPages ? 'Back to Trip Approvals' : 'Next'}
+              </button>
+            </Box>
+          </Box>
+          <Modal
+            centered
+            show={this.state.showModal}
+            onHide={this.closeModal}
+          >
+            <div className="trip-details-close-button">
+              <i className="material-icons close-button" onClick={this.closeModal} role="button" tabIndex={0}>close</i>
             </div>
-            <div className="ovr-modal-button-container">
-              <button type="button" className="vrf-submit-button signup-button" onClick={this.copyEmail}>Copy email address</button>
-            </div>
-          </div>
-        </Modal>
-      </Box>
+            <Badge type="denied" />
+            {/* <img className="status-badge ovr-status-badge" src={this.badges.denied} alt="warning_badge" /> */}
 
-    );
+            <div className="cancel-content">
+              <p className="cancel-question">{`Contact ${this.props.trip.leaders[0].name}`}</p>
+              <p className="cancel-message">
+                Please email the requester if you need them to update a request. They can't update a request after it has been reviewed.
+              </p>
+              <div className="ovr-modal-button-container">
+                <input
+                  ref={this.emailRef}
+                  type="text"
+                  className="ovr-requester-email"
+                  defaultValue={this.props.trip.leaders[0].email}
+                />
+              </div>
+              <div className="ovr-modal-button-container">
+                <button type="button" className="vrf-submit-button signup-button" onClick={this.copyEmail}>Copy email address</button>
+              </div>
+            </div>
+          </Modal>
+        </Box>
+      );
+    } else return (<Loading type="doc" />);
   }
 }
 const mapStateToProps = (state) => {
@@ -317,4 +323,4 @@ const mapStateToProps = (state) => {
     trip: state.trips.trip,
   };
 };
-export default withRouter(connect(mapStateToProps, { reviewGearRequest, reviewTrippeeGearRequest, reviewPCardRequests, appError })(OPOTripDetails));
+export default withRouter(connect(mapStateToProps, { fetchTrip, reviewGearRequest, reviewTrippeeGearRequest, reviewPCardRequests, appError })(OPOTripApproval));
