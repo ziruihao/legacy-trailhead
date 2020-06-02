c4ee8b1ba2702c97b52962b2c04222b0d4dd3238
ziruihao <ziray.hao.22@dartmouth.edu>
2020-06-02 18:28:02 -0400

    added more badges to Badge component

--- src/components/badge/badge.js
+++ src/components/badge/badge.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import approved from './approved-badge.svg';
-import denied from './denied-badge.svg';
-import leader from './leader-badge.svg';
-import pending from './pending-badge.svg';
-import warning from './warning-badge.svg';
+import approved from './badges/approved-badge.svg';
+import denied from './badges/denied-badge.svg';
+import leader from './badges/leader-badge.svg';
+import pending from './badges/pending-badge.svg';
+import warning from './badges/warning-badge.svg';
+import add from './badges/add-badge.svg';
+import id from './badges/id-badge.svg';
+import list from './badges/list-badge.svg';
+import marker from './badges/marker-badge.svg';
+import people from './badges/people-badge.svg';
+import person from './badges/person-badge.svg';
+import trips from './badges/trips-badge.svg';
+import vehicle from './badges/vehicle-badge.svg';
+import verified from './badges/verified-badge.svg';
+import calendar from './badges/calendar-badge.svg';
+
 import './badge.scss';
 
 const Badge = (props) => {
@@ -20,6 +31,26 @@ const Badge = (props) => {
       return (<img className="doc-badge" alt="pending badge" style={style} src={pending} data-tip={props.dataTip} data-for={props.dataFor} />);
     case 'warning':
       return (<img className="doc-badge" alt="warning badge" style={style} src={warning} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'add':
+      return (<img className="doc-badge" alt="add badge" style={style} src={add} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'id':
+      return (<img className="doc-badge" alt="id badge" style={style} src={id} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'list':
+      return (<img className="doc-badge" alt="list badge" style={style} src={list} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'marker':
+      return (<img className="doc-badge" alt="marker badge" style={style} src={marker} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'people':
+      return (<img className="doc-badge" alt="people badge" style={style} src={people} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'person':
+      return (<img className="doc-badge" alt="person badge" style={style} src={person} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'trips':
+      return (<img className="doc-badge" alt="trips badge" style={style} src={trips} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'vehicle':
+      return (<img className="doc-badge" alt="vehicle badge" style={style} src={vehicle} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'verified':
+      return (<img className="doc-badge" alt="verified badge" style={style} src={verified} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'calendar':
+      return (<img className="doc-badge" alt="calendar badge" style={style} src={calendar} data-tip={props.dataTip} data-for={props.dataFor} />);
     default:
       return null;
   }
