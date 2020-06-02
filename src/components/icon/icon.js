3cc94b24d62b4938f6acb7c877901744fcef0a26
ziruihao <ziray.hao.22@dartmouth.edu>
2020-06-02 18:26:51 -0400

    built Icon component

---
+++ src/components/icon/icon.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import edit from './icons/edit-icon.svg';
+import email from './icons/email-icon.svg';
+import phone from './icons/phone-icon.svg';
+import save from './icons/save-icon.svg';
+import warning from './icons/warning-icon.svg';
+
+import './icon.scss';
+
+const Icon = (props) => {
+  const id = props.id || null;
+  const measure = props.measure || 'px';
+  const style = { width: `${props.size ? `${props.size}${measure}` : '100%'}` };
+  switch (props.type) {
+    case 'approved':
+      return (<img id={id} className="doc-icon" alt="approved icon" style={style} src={edit} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'denied':
+      return (<img id={id} className="doc-icon" alt="denied icon" style={style} src={email} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'leader':
+      return (<img id={id} className="doc-icon" alt="leader icon" style={style} src={phone} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'pending':
+      return (<img id={id} className="doc-icon" alt="pending icon" style={style} src={save} data-tip={props.dataTip} data-for={props.dataFor} />);
+    case 'warning':
+      return (<img id={id} className="doc-icon" alt="warning icon" style={style} src={warning} data-tip={props.dataTip} data-for={props.dataFor} />);
+    default:
+      return null;
+  }
+};
+
+export default Icon;
