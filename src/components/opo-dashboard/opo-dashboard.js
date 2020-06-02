a91a4cf46e44e95c60af0f79ecbb29c8ccd590a8
ziruihao <ziray.hao.22@dartmouth.edu>
2020-06-02 18:27:37 -0400

    redesigned dashboard

--- src/components/opo-dashboard/opo-dashboard.js
+++ src/components/opo-dashboard/opo-dashboard.js
@@ -1,33 +1,99 @@
 import React from 'react';
 import { withRouter, NavLink } from 'react-router-dom';
-import TripApprovals from '../../img/mountain.svg';
-import VehicleRequests from '../../img/vehicle.svg';
-import LeaderApproval from '../../img/leader.svg';
-import OPOAssignments from '../../img/mytrips_icon.png';
+import { Stack, Queue, Divider, Box } from '../layout';
+import Badge from '../badge';
+import OPOTrips from '../opo-approvals/trips';
+import OPOVehicleRequests from '../opo-approvals/vehicle-requests';
+import OPOLeaders from '../opo-approvals/leaders';
+import FleetManagement from '../fleet-management';
+import VehicleCalendar from '../vehicleCalendar';
 import './opo-dashboard.scss';
 
-const OPODashboard = () => {
+const OPODashboard = (props) => {
+  let dashboardContent = <OPOTrips />;
+  const active = { trips: false, vehicles: false, calendar: false, leaders: false, fleet: false };
+  switch (props.location.pathname) {
+    case '/opo-dashboard/opo-trips':
+      active.trips = true;
+      break;
+    case '/opo-dashboard/vehicle-requests':
+      dashboardContent = <OPOVehicleRequests />;
+      active.vehicles = true;
+      break;
+    case '/opo-dashboard/vehicle-calendar':
+      dashboardContent = <VehicleCalendar />;
+      active.calendar = true;
+      break;
+    case '/opo-dashboard/leader-approvals':
+      dashboardContent = <OPOLeaders />;
+      active.leaders = true;
+      break;
+    case '/opo-dashboard/opo-fleet-management':
+      dashboardContent = <FleetManagement />;
+      active.fleet = true;
+      break;
+    default:
+      dashboardContent = <OPOTrips />;
+  }
   return (
-    <div id="dashboard" className="center-view">
-      <div id="dashboard-tiles">
-        <NavLink className="section" to="/opo-trips">
-          <img src={TripApprovals} alt="Trip Approval Icon" />
-          <div className="titles doc-h2">Trip Approvals</div>
+    <Box id="dashboard" dir="col" align="stretch" expand>
+      <Box id="dashboard-tiles" dir="row" justify="start" align="stretch" pad={50}>
+        <NavLink className={`section ${active.trips ? 'current-section' : ''}`} to="/opo-dashboard/opo-trips">
+          <Box dir="row" align="center">
+            <Badge type="trips" size={48} />
+            <Queue size={25} />
+            <div className="titles doc-h2">Trip Approvals</div>
+          </Box>
         </NavLink>
-        <NavLink className="section" to="/vehicle-requests">
-          <img src={VehicleRequests} alt="Vehicle Requests Icon" />
-          <div className="titles doc-h2">Vehicle Requests</div>
+        <Queue size={50} />
+        <Divider dir="col" size={1} />
+        <Queue size={50} />
+        <NavLink className={`section ${active.vehicles ? 'current-section' : ''}`} to="/opo-dashboard/vehicle-requests">
+          <Box dir="row" align="center">
+            <Badge type="vehicle" size={48} />
+            <Queue size={25} />
+            <div className="titles doc-h2">Vehicle Requests</div>
+          </Box>
         </NavLink>
-        <NavLink className="section" to="/leader-approvals">
-          <img src={LeaderApproval} alt="Leader Icon" />
-          <div className="titles doc-h2">Profile Approvals</div>
+        <Queue size={50} />
+        <Divider dir="col" size={1} />
+        <Queue size={50} />
+        <NavLink className={`section ${active.calendar ? 'current-section' : ''}`} to="/opo-dashboard/vehicle-calendar">
+          <Box dir="row" align="center">
+            <Badge type="calendar" size={48} />
+            <Queue size={25} />
+            <div className="titles doc-h2">Calendar</div>
+          </Box>
         </NavLink>
-        <NavLink className="section" to="/opo-fleet-management">
-          <img src={OPOAssignments} alt="Fleet Icon" />
-          <div className="titles doc-h2">Manage Fleet</div>
+        <Queue size={50} />
+        <Divider dir="col" size={1} />
+        <Queue size={50} />
+        <NavLink className={`section ${active.leaders ? 'current-section' : ''}`} to="/opo-dashboard/leader-approvals">
+          <Box dir="row" align="center">
+            <Badge type="person" size={48} />
+            <Queue size={25} />
+            <div className="titles doc-h2">Profile Approvals</div>
+          </Box>
         </NavLink>
-      </div>
-    </div>
+        <Queue size={50} />
+        <Divider dir="col" size={1} />
+        <Queue size={50} />
+        <NavLink className={`section ${active.fleet ? 'current-section' : ''}`} to="/opo-dashboard/opo-fleet-management">
+          <Box dir="row" align="center">
+            <Badge type="marker" size={48} />
+            <Queue size={25} />
+            <div className="titles doc-h2">Manage Fleet</div>
+          </Box>
+        </NavLink>
+        <Queue size={50} />
+        <Divider dir="col" size={1} />
+        <Queue size={50} />
+      </Box>
+      <Stack size={50} />
+      <Box id="dashboard-content" dir="col" align="stretch" expand>
+        {dashboardContent}
+      </Box>
+    </Box>
   );
 };
 
