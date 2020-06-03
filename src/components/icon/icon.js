c336ff2e5996439c3e9a2d2fc677761585ceb758
ziruihao <ziray.hao.22@dartmouth.edu>
2020-06-03 18:42:38 -0400

    added more icons

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -4,6 +4,14 @@ import email from './icons/email-icon.svg';
 import phone from './icons/phone-icon.svg';
 import save from './icons/save-icon.svg';
 import warning from './icons/warning-icon.svg';
+import check from './icons/check-icon.svg';
+import close from './icons/close-icon.svg';
+import date from './icons/date-icon.svg';
+import doc from './icons/doc-icon.svg';
+import dropdown from './icons/dropdown-icon.svg';
+import more from './icons/more-icon.svg';
+import tree from './icons/tree-icon.svg';
+import mountain from './icons/mountain-icon.svg';
 
 import './icon.scss';
 
@@ -12,16 +20,30 @@ const Icon = (props) => {
   const measure = props.measure || 'px';
   const style = { width: `${props.size ? `${props.size}${measure}` : '100%'}` };
   switch (props.type) {
-    case 'approved':
+    case 'edit':
       return (<img id={id} className="doc-icon" alt="approved icon" style={style} src={edit} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
-    case 'denied':
+    case 'email':
       return (<img id={id} className="doc-icon" alt="denied icon" style={style} src={email} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
-    case 'leader':
+    case 'phone':
       return (<img id={id} className="doc-icon" alt="leader icon" style={style} src={phone} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
-    case 'pending':
+    case 'save':
       return (<img id={id} className="doc-icon" alt="pending icon" style={style} src={save} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
     case 'warning':
       return (<img id={id} className="doc-icon" alt="warning icon" style={style} src={warning} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
+    case 'check':
+      return (<img id={id} className="doc-icon" alt="approved icon" style={style} src={check} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
+    case 'close':
+      return (<img id={id} className="doc-icon" alt="denied icon" style={style} src={close} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
+    case 'date':
+      return (<img id={id} className="doc-icon" alt="leader icon" style={style} src={date} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
+    case 'doc':
+      return (<img id={id} className="doc-icon" alt="pending icon" style={style} src={doc} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
+    case 'dropdown':
+      return (<img id={id} className="doc-icon" alt="warning icon" style={style} src={dropdown} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
+    case 'tree':
+      return (<img id={id} className="doc-icon" alt="warning icon" style={style} src={tree} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
+    case 'mountain':
+      return (<img id={id} className="doc-icon" alt="warning icon" style={style} src={mountain} data-tip={props.dataTip} data-for={props.dataFor} onClick={props.onClick} />);
     default:
       return null;
   }
