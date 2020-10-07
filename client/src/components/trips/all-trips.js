1d5fd9ba3d684f9afef0b554306ecc40003ca545
ziruihao <ziray.hao.22@dartmouth.edu>
2020-10-07 17:54:59 -0600

    Added filters button to AllTrips

--- client/src/components/trips/all-trips.js
+++ client/src/components/trips/all-trips.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink, withRouter } from 'react-router-dom';
@@ -8,6 +7,8 @@ import TripDetailsBasic from '../trip-details/basic/trip-details-basic';
 import TripCard from '../trip-card';
 import Toggle from '../toggle';
 import Text from '../text';
+import Icon from '../icon';
+import Button from '../button';
 import DOCLoading from '../doc-loading';
 import { fetchTrips, fetchTrip, getClubs, leaveTrip } from '../../actions';
 import utils from '../../utils';
@@ -27,27 +28,27 @@ class AllTrips extends Component {
         'Next 3 days',
         'This week',
         'This weekend',
-        'In a month'
+        'In a month',
       ],
       selectedTimePeriod: 'This week',
-      grid: true,
       showTrip: false,
       showCancellationModal: false,
       cancelling: false,
       startDate: null,
       seePastTrips: false,
+      showFilters: false,
     };
   }
 
-  componentDidMount(props) {
+  componentDidMount() {
     this.props.fetchTrips();
     this.props.getClubs();
   }
 
   cancelSignup = () => {
-    this.setState({cancelling: true});
+    this.setState({ cancelling: true });
     this.props.leaveTrip(this.props.trip._id, this.props.user._id).then(() => {
-      this.setState({cancelling: false, showCancellationModal: false});
+      this.setState({ cancelling: false, showCancellationModal: false });
     });
   }
 
@@ -60,12 +61,12 @@ class AllTrips extends Component {
   }
 
   formatDescription = (des) => {
-      let description = des;
-      if(description.length > 100){
-        description = description.substring(0, 101) + "...";
-      }
-      return description;
-}
+    let description = des;
+    if (description.length > 100) {
+      description = `${description.substring(0, 101)}...`;
+    }
+    return description;
+  }
 
   compareStartDates = (a, b) => {
     const t1 = new Date(a.startDate);
@@ -75,62 +76,68 @@ class AllTrips extends Component {
 
   renderClubDropdown = () => {
     return (
-      <Dropdown onSelect={eventKey => this.setState({club: eventKey})}>
-        <Dropdown.Toggle className="field">
-          <span className="field-dropdown-bootstrap">{this.state.club}</span>
-          <img className="dropdown-icon" src={dropdownIcon} alt="dropdown-toggle" />
+      <Dropdown onSelect={eventKey => this.setState({ club: eventKey })}>
+        <Dropdown.Toggle className='field'>
+          <span className='field-dropdown-bootstrap'>{this.state.club}</span>
+          <img className='dropdown-icon' src={dropdownIcon} alt='dropdown-toggle' />
         </Dropdown.Toggle>
-        <Dropdown.Menu className="field-dropdown-menu">
-          <Dropdown.Item eventKey="All clubs">All clubs</Dropdown.Item>
+        <Dropdown.Menu className='field-dropdown-menu'>
+          <Dropdown.Item eventKey='All clubs'>All clubs</Dropdown.Item>
           <Dropdown.Divider />
-            {this.props.clubs.map((club => {
-              return (<Dropdown.Item key={club._id} eventKey={club.name}>{club.name}</Dropdown.Item>);
-            }))}
+          {this.props.clubs.map(((club) => {
+            return (<Dropdown.Item key={club._id} eventKey={club.name}>{club.name}</Dropdown.Item>);
+          }))}
         </Dropdown.Menu>
       </Dropdown>
-    )
+    );
   }
 
   renderTimePeriodDropdown = () => {
     return (
-      <Dropdown onSelect={eventKey => {
-        if (eventKey !== 'Specific day') this.setState({startDate: null})
-        this.setState({selectedTimePeriod: eventKey})
-        }}>
-      <Dropdown.Toggle className="field">
-        <span className="field-dropdown-bootstrap">{this.state.selectedTimePeriod}</span>
-        <img className="dropdown-icon" src={dropdownIcon} alt="dropdown-toggle" />
-      </Dropdown.Toggle>
-      <Dropdown.Menu className="field-dropdown-menu">
-        {this.state.timePeriods.map((timePeriod => {
-          return (<Dropdown.Item key={timePeriod} eventKey={timePeriod}>{timePeriod}</Dropdown.Item>);
-        }))}
-        <Dropdown.Divider />
-        <Dropdown.Item eventKey="Specific day">Specific day</Dropdown.Item>
-        <Dropdown.Item eventKey="All">All</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-    )
+      <Dropdown onSelect={(eventKey) => {
+        if (eventKey !== 'Specific day') this.setState({ startDate: null });
+        this.setState({ selectedTimePeriod: eventKey });
+      }}
+      >
+        <Dropdown.Toggle className='field'>
+          <span className='field-dropdown-bootstrap'>{this.state.selectedTimePeriod}</span>
+          <img className='dropdown-icon' src={dropdownIcon} alt='dropdown-toggle' />
+        </Dropdown.Toggle>
+        <Dropdown.Menu className='field-dropdown-menu'>
+          {this.state.timePeriods.map(((timePeriod) => {
+            return (<Dropdown.Item key={timePeriod} eventKey={timePeriod}>{timePeriod}</Dropdown.Item>);
+          }))}
+          <Dropdown.Divider />
+          <Dropdown.Item eventKey='Specific day'>Specific day</Dropdown.Item>
+          <Dropdown.Item eventKey='All'>All</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
   }
 
   renderStartDropdown = () => {
-    return(
-      <input type="date" name="startDate" onChange={(e) =>{
-        this.setState({ startDate: e.target.value }); 
-      }} className="field all-trips-date-select" value={this.state.startDate} />
+    return (
+      <input type='date'
+        name='startDate'
+        onChange={(e) => {
+          this.setState({ startDate: e.target.value });
+        }}
+        className='field all-trips-date-select'
+        value={this.state.startDate}
+      />
     );
   }
 
   setCurrTrip = (trip) => {
     this.props.fetchTrip(trip._id).then(() => {
-      this.setState({showTrip: true});
+      this.setState({ showTrip: true });
     });
   }
 
   renderTrips = () => {
-    let tripsFilteringProcess = [this.props.trips];
+    const tripsFilteringProcess = [this.props.trips];
 
-    switch(this.state.selectedTimePeriod) {
+    switch (this.state.selectedTimePeriod) {
       case 'All':
         tripsFilteringProcess.push(tripsFilteringProcess.pop().filter(trip => !utils.dates.inThePast(trip.startDateAndTime)));
         break;
@@ -154,82 +161,110 @@ class AllTrips extends Component {
     const filteredTrips = tripsFilteringProcess.pop().sort(this.compareStartDates);
 
     if (filteredTrips.length === 0) {
-      return <div id="trips-page-not-found">
-        <img src={sadTree} ></img>
-        <div className="h2">Sorry, we couldn't find any!</div>
-        </div>;
+      return (
+        <div id='trips-page-not-found'>
+          <img src={sadTree} alt='sad tree' />
+          <div className='h2'>Sorry, we couldn&apos;t find any!</div>
+        </div>
+      );
     } else {
-      return filteredTrips.map((trip) => <TripCard type='large' trip={trip} displayInfoBadges user={this.props.user} displayDescription  onClick={() => this.setCurrTrip(trip)} key={trip._id} />);
+      return filteredTrips.map(trip => <TripCard type='large' trip={trip} displayInfoBadges user={this.props.user} displayDescription onClick={() => this.setCurrTrip(trip)} key={trip._id} />);
     }
   }
 
   render() {
-      return (
-        <div id="trips-page" className="center-view spacy">
-          <div className="doc-card">
-            <Box dir="col" pad={25}>
-              <div className="doc-h1">Explore trips</div>
-              <Stack size={15}></Stack>
-              <div id="trip-safari-configs">
-                {this.renderClubDropdown()}
-                {this.renderTimePeriodDropdown()}
-                {this.state.selectedTimePeriod === 'Specific day' ? this.renderStartDropdown() : null}
-                <Toggle value={this.state.beginnerOnly} id="defaultCheck2" label="Beginner only" onChange={(e) => {
-                  this.setState(prevState => {
-                    return {beginnerOnly: !prevState.beginnerOnly}
-                  })}} disabled={false}></Toggle>
-                <Toggle value={this.state.seePastTrips} id="defaultCheck1" label="See past trips" onChange={(e) => {
-                  this.setState(prevState => {
-                    return {seePastTrips: !prevState.seePastTrips}
-                  })
-                }} disabled={false}></Toggle>
-              </div>
-            </Box>
-          </div>
-          <div id="trip-tiles">
-            {this.renderTrips()}
-          </div>
-          <Modal
-            centered
-            show={this.state.showCancellationModal}
-            onHide={() => this.setState({ showCancellationModal: false })}
-          >
-            <Box id="trip-cancel-modal" dir="col" align="center" pad={25}>
-              {this.state.cancelling ? 
-                <div id="trip-cancel-modal-loading">
-                  <DOCLoading type="spin" width="35" height="35" measure="px" />
-                </div>
-                :
-                null
-              }
-              <img src={sadTree} alt="confirm-cancel" className="cancel-image" />
-              <Stack size={24} />
-              <Text type='h2'>Are you sure you want to cancel?</Text>
-              <Stack size={24} />
-              <div className="p1 center-text">This cute tree will die if you do and you’ll have to register for this trip again if you change your mind. Don't worry - we inform the trip leaders know so you don't have to.</div>
-              <Stack size={24} />
-              <Box dir="row" justify="center">
-                <div className="doc-button" onClick={() => this.setState({ showCancellationModal: false, showTrip: true, })} role="button" tabIndex={0}>Wait no</div>
-                <Queue size={15} />
-                <div className="doc-button alarm" onClick={this.cancelSignup} role="button" tabIndex={0}>Remove me</div>
-              </Box>
-            </Box>
-          </Modal>
-          <Modal
-            centered
-            size="lg"
-            show={this.state.showTrip}
-            onHide={() => this.setState({showTrip: false})}
-          >
-            <div id="trip-details-modal-wrapper">
-              <TripDetailsBasic
-                closeModal = {() => this.setState({showTrip: false})}
-                openCancellationModal={() => this.setState({ showCancellationModal: true, showTrip: false })} hideCancellationModal={() => this.setState({ showCancellationModal: false })} />
-            </div>
-          </Modal>
+    return (
+      <div id='trips-page' className='center-view spacy'>
+        <Box id='trip-safari-menu' dir='col' pad={25} className='doc-card'>
+          <Box dir='row' justify='between' align='center'>
+            <Text type='h1'>Explore trips</Text>
+            <Button type='toggle' color='green' onClick={() => { this.setState(prevState => ({ showFilters: !prevState.showFilters })); }} active={this.state.showFilters}>
+              <Icon type='filter' size={18} />
+              <Queue size={9} />
+              <Text type='button'>Filters</Text>
+            </Button>
+          </Box>
+          {this.state.showFilters
+            ? (
+              <>
+                <Stack size={25} />
+                <Box dir='row' justify='between' align='center' id='trip-safari-configs'>
+                  {this.renderClubDropdown()}
+                  {this.renderTimePeriodDropdown()}
+                  {this.state.selectedTimePeriod === 'Specific day' ? this.renderStartDropdown() : null}
+                  <Toggle value={this.state.beginnerOnly}
+                    id='defaultCheck2'
+                    label='Beginner only'
+                    onChange={() => {
+                      this.setState((prevState) => {
+                        return { beginnerOnly: !prevState.beginnerOnly };
+                      });
+                    }}
+                    disabled={false}
+                  />
+                  <Toggle value={this.state.seePastTrips}
+                    id='defaultCheck1'
+                    label='See past trips'
+                    onChange={() => {
+                      this.setState((prevState) => {
+                        return { seePastTrips: !prevState.seePastTrips };
+                      });
+                    }}
+                    disabled={false}
+                  />
+                </Box>
+              </>
+            )
+            : null
+          }
+        </Box>
+        <div id='trip-tiles'>
+          {this.renderTrips()}
         </div>
-      );
-    }
+        <Modal
+          centered
+          show={this.state.showCancellationModal}
+          onHide={() => this.setState({ showCancellationModal: false })}
+        >
+          <Box id='trip-cancel-modal' dir='col' align='center' pad={25}>
+            {this.state.cancelling
+              ? (
+                <div id='trip-cancel-modal-loading'>
+                  <DOCLoading type='spin' width='35' height='35' measure='px' />
+                </div>
+              )
+              : null
+              }
+            <img src={sadTree} alt='confirm-cancel' className='cancel-image' />
+            <Stack size={24} />
+            <Text type='h2'>Are you sure you want to cancel?</Text>
+            <Stack size={24} />
+            <div className='p1 center-text'>This cute tree will die if you do and you’ll have to register for this trip again if you change your mind. Don&apos;t worry - we inform the trip leaders know so you don&apos;t have to.</div>
+            <Stack size={24} />
+            <Box dir='row' justify='center'>
+              <div className='doc-button' onClick={() => this.setState({ showCancellationModal: false, showTrip: true })} role='button' tabIndex={0}>Wait no</div>
+              <Queue size={15} />
+              <div className='doc-button alarm' onClick={this.cancelSignup} role='button' tabIndex={0}>Remove me</div>
+            </Box>
+          </Box>
+        </Modal>
+        <Modal
+          centered
+          size='lg'
+          show={this.state.showTrip}
+          onHide={() => this.setState({ showTrip: false })}
+        >
+          <div id='trip-details-modal-wrapper'>
+            <TripDetailsBasic
+              closeModal={() => this.setState({ showTrip: false })}
+              openCancellationModal={() => this.setState({ showCancellationModal: true, showTrip: false })}
+              hideCancellationModal={() => this.setState({ showCancellationModal: false })}
+            />
+          </div>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 
