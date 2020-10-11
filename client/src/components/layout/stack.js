e0e873cb7fc7e97a2f3b23bbde5348d0f2c9cb9e
ziruihao <ziray.hao.22@dartmouth.edu>
2020-10-10 23:28:59 -0600

    Fixed bug with Stack minSize

--- client/src/components/layout/stack.js
+++ client/src/components/layout/stack.js
@@ -4,11 +4,12 @@ import './layout.scss';
 const Stack = (props) => {
   const style = { height: null, minHeight: null };
   if (props.minSize) {
-    if (typeof props.size === 'number') {
-      style.minHeight = `${props.size}px`;
-    } else if (typeof props.size === 'string') {
+    style.flex = 1;
+    if (typeof props.minSize === 'number') {
+      style.minHeight = `${props.minSize}px`;
+    } else if (typeof props.minSize === 'string') {
       // eslint-disable-next-line prefer-destructuring
-      style.minHeight = props.size;
+      style.minHeight = props.minSize;
     }
   } else if (props.size) {
     if (typeof props.size === 'number') {
