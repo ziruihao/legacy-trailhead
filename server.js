783153c1000c7823f468d0a09060a99d85b4e9cd
ziruihao <ziray.hao.22@dartmouth.edu>
2021-05-01 00:08:30 -0600

    Refactor to enable Netlify deployment

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/api/getList', (req,res) => {
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) =>{
-	res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+	res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 const port = process.env.PORT || 8080;
